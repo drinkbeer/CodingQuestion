0bb09c6e724f0757d74860128a1b94bb79818898
Jason Chen <chenjb90@gmail.com>
2019-04-27 15:51:51 -0700

    Update LeetCode-53-Maximum-Subarray.java with a divide and conquer solution

--- src/LeetCode-53-Maximum-Subarray.java
+++ src/LeetCode-53-Maximum-Subarray.java
@@ -107,4 +107,36 @@ public class Solution {
     }
     
     
+        // 7. Divide & Conquer
+    /*
+    The Divide-and-Conquer algorithm breaks nums into two halves and find the maximum subarray sum in them recursively. Well, the most tricky part is to handle the case that the maximum subarray spans the two halves. For this case, we use a linear algorithm: starting from the middle element and move to both ends (left and right ends), record the maximum sum we have seen. In this case, the maximum sum is finally equal to the middle element plus the maximum sum of moving leftwards and the maximum sum of moving rightwards.
+    */
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        
+        return maxSubArray(nums, 0, nums.length - 1);
+    }
+    
+    private int maxSubArray(int[] nums, int l, int r) {
+        if (l == r) return nums[l];
+        
+        int sum = 0, leftSum = Integer.MIN_VALUE, rightSum = Integer.MIN_VALUE;
+        int m = l + (r - l) / 2;
+        
+        for (int i = m; i >= l; i--) {
+            sum += nums[i];
+            leftSum = Math.max(leftSum, sum);
+        }
+        
+        sum = 0;
+        for (int i = m + 1; i <= r; i++) {
+            sum += nums[i];
+            rightSum = Math.max(rightSum, sum);
+        }
+        
+        return Math.max(leftSum + rightSum, Math.max(maxSubArray(nums, l, m), maxSubArray(nums, m + 1, r)));
+    }
+    
+    
 }
