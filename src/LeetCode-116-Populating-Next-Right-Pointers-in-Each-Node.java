31fe012da60f5b01a9a7fabf3edd3dcd566e75bf
Jason Chen <chenjb90@gmail.com>
2019-04-29 00:15:14 -0700

    Update LeetCode-116-Populating-Next-Right-Pointers-in-Each-Node.java

--- src/LeetCode-116-Populating-Next-Right-Pointers-in-Each-Node.java
+++ src/LeetCode-116-Populating-Next-Right-Pointers-in-Each-Node.java
@@ -69,4 +69,20 @@ public class Solution {
         
         return root;
     }
+    
+    // 2. DFS
+    // https://leetcode.com/problems/populating-next-right-pointers-in-each-node/discuss/37473/My-recursive-solution(Java)
+    public Node connect(Node root) {
+        if(root == null) return null;
+
+        if(root.left != null) {
+            root.left.next = root.right;
+            if(root.next != null) root.right.next = root.next.left;
+        }
+
+        connect(root.left);
+        connect(root.right);
+        
+        return root;
+    }
 }
