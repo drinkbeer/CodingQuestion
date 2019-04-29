291d8c7f77fdf27043a75a653423569f568d2f2c
Jason Chen <chenjb90@gmail.com>
2019-04-28 17:38:20 -0700

    Update LeetCode-106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal.java

--- src/LeetCode-106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal.java
+++ src/LeetCode-106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal.java
@@ -47,3 +47,63 @@ public class Solution {
         return -1;
     }
 }
+
+/*
+
+preorder    root | left tree | right tree
+inorder     left tree | root | right tree
+postorder   left tree | right tree | root
+
+1.Using the postorder to find the root position, and using the root to split the left tree, and right tree in inorder.
+
+2.Construct tree
+
+root.left = construct(inorder(left tree), postorder(left tree))
+root.right = construct(inorder(right tree), postorder(right tree))
+
+
+1, 2, 0, 3, 4
+
+1, 2, 3, 4, 0
+
+
+*/
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (inorder == null || postorder == null) return null;
+        if (inorder.length != postorder.length) return null;
+        
+        return buildTree(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+    }
+    
+    // Helper function to construct the tree recursively.
+    private TreeNode buildTree(int[] inorder, int inStart, int inEnd, int[] postorder, int poStart, int poEnd) {
+        if (inStart > inEnd || poStart > poEnd) return null;
+        
+        // find the position of root in inorder
+        // root index is postorder[poEnd]
+        int position = findPosition(inorder, inStart, inEnd, postorder[poEnd]);
+        
+        // construct root node
+        TreeNode root = new TreeNode(postorder[poEnd]);
+        
+        // calculate left tree size
+        int leftSize = position - inStart;
+        int rightSize = inEnd - position;
+        
+        root.left = buildTree(inorder, inStart, inStart + leftSize - 1, postorder, poStart, poStart + leftSize - 1);
+        root.right = buildTree(inorder, position + 1, inEnd, postorder, poStart + leftSize, poEnd - 1);
+        
+        return root;
+    }
+    
+    private int findPosition(int[] arr, int start, int end, int target) {
+        if (start < 0 || end > arr.length - 1) return -1;
+        for (int i = start; i <= end; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
