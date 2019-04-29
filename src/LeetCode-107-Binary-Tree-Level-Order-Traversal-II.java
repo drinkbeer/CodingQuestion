a67d73811ddd0aec2fa9a4b23d5325326bb3ec9f
Jason Chen <chenjb90@gmail.com>
2019-04-28 18:11:12 -0700

    Update LeetCode-107-Binary-Tree-Level-Order-Traversal-II.java

--- src/LeetCode-107-Binary-Tree-Level-Order-Traversal-II.java
+++ src/LeetCode-107-Binary-Tree-Level-Order-Traversal-II.java
@@ -49,3 +49,118 @@ public class Solution {
         
     }
 }
+
+class Solution {
+    // 1. BFS (by reversing the order of result)
+//     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+//         List<List<Integer>> result = new ArrayList<List<Integer>>();
+//         if (root == null) return result;
+        
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root);
+        
+//         while(!queue.isEmpty()) {
+//             // Process one level
+//             int size = queue.size();
+//             List<Integer> level = new ArrayList<>();
+            
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode curr = queue.poll();
+//                 if (curr.left != null) queue.add(curr.left);
+//                 if (curr.right != null) queue.add(curr.right);
+//                 level.add(curr.val);
+//             }
+            
+//             result.add(level);
+//         }
+        
+//         // reverse the result
+//         int resultSize = result.size();
+//         for (int i = resultSize - 1; i >= 0; i--) { 
+//             result.add(result.remove(i));
+//         }
+        
+//         return result;
+//     }
+    
+        // 2. BFS (by adding level to the beginning of the result)
+//     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+//         List<List<Integer>> result = new ArrayList<List<Integer>>();
+//         if (root == null) return result;
+        
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root);
+        
+//         while(!queue.isEmpty()) {
+//             // Process one level
+//             int size = queue.size();
+//             List<Integer> level = new ArrayList<>();
+            
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode curr = queue.poll();
+//                 if (curr.left != null) queue.add(curr.left);
+//                 if (curr.right != null) queue.add(curr.right);
+//                 level.add(curr.val);
+//             }
+            
+//             result.add(0, level);
+//         }
+        
+//         return result;
+//     }
+    
+    // 3. DFS (get the normal order, and reverse it)
+//     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+//         List<List<Integer>> result = new ArrayList<List<Integer>>();
+//         if (root == null) return result;
+        
+//         levelOrderBottom(root, 0, result);
+        
+//         // reverse the order of the result
+//         int resultSize = result.size();
+//         for (int i = resultSize - 1; i >= 0; i--) {
+//             result.add(result.remove(i));
+//         }
+        
+//         return result;
+//     }
+    
+//     // level starts from 0
+//     private void levelOrderBottom(TreeNode node, int level, List<List<Integer>> result) {
+//         if (result.size() == level) {
+//             result.add(new ArrayList<Integer>());
+//         }
+        
+//         result.get(level).add(node.val);
+//         if (node.left != null) levelOrderBottom(node.left, level + 1, result);
+//         if (node.right != null) levelOrderBottom(node.right, level + 1, result);
+        
+//         // the recursive loop ends when left and right are both null
+//     }
+    
+        // 4. DFS (by adding level to the beginning of the result)
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) return result;
+        
+        levelOrderBottom(root, 0, result);
+        
+        return result;
+    }
+    
+    // level starts from 0
+    private void levelOrderBottom(TreeNode node, int level, List<List<Integer>> result) {
+        if (result.size() == level) {
+            result.add(0, new ArrayList<Integer>());
+        }
+        
+        
+        // for instance, a two level result.
+        // [[1,2],[0]]
+        result.get(result.size() - level - 1).add(node.val);
+        if (node.left != null) levelOrderBottom(node.left, level + 1, result);
+        if (node.right != null) levelOrderBottom(node.right, level + 1, result);
+        
+        // the recursive loop ends when left and right are both null
+    }
+}
