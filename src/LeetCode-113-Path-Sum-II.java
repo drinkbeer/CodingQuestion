56fb1651779cc96460db55a704ece8a5bdf8ca7f
Jason Chen <chenjb90@gmail.com>
2019-04-28 19:41:32 -0700

    Update LeetCode-113-Path-Sum-II.java

--- src/LeetCode-113-Path-Sum-II.java
+++ src/LeetCode-113-Path-Sum-II.java
@@ -49,3 +49,78 @@ public class Solution {
     }
     
 }
+
+
+class Solution {
+    // 1.DFS
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) return result;
+        
+        List<Integer> list = new ArrayList<>();
+        list.add(root.val);
+        
+        pathSum(root, sum - root.val, result, list);
+        
+        return result;
+    }
+    
+    private void pathSum(TreeNode node, int sum, List<List<Integer>> result, List<Integer> list) {
+        if (node.left == null && node.right == null && sum == 0) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        
+        if (node.left != null) {
+            list.add(node.left.val);
+            pathSum(node.left, sum - node.left.val, result, list);
+            list.remove(list.size() - 1);
+        }
+        
+        if (node.right != null) {
+            list.add(node.right.val);
+            pathSum(node.right, sum - node.right.val, result, list);
+            list.remove(list.size() - 1);
+        }
+    }
+    
+    
+    // 2.DFS (a bit complex)
+//     public List<List<Integer>> pathSum(TreeNode root, int sum) {
+//         List<List<Integer>> result = new ArrayList<List<Integer>>();
+//         if (root == null) return result;
+        
+//         pathSum(root, sum, result, new ArrayList<Integer>());
+//         return result;
+//     }
+    
+//     private void pathSum(TreeNode node, int sum, List<List<Integer>> result, List<Integer> list) {
+//         if (node.left == null && node.right == null) {
+//             // node is a leaf
+//             if (sum == node.val) {
+//                 list.add(node.val);
+//                 result.add(new ArrayList<Integer>(list));
+//                 list.remove(list.size() - 1);
+//             }
+            
+//             return;
+//         }
+        
+//         // node is not a leaf, proceed to next level
+//         list.add(node.val);
+//         if (node.left != null) {
+//             pathSum(node.left, sum - node.val, result, list);
+//         }
+//         if (node.right != null) {
+//             pathSum(node.right, sum - node.val, result, list);
+//         }
+//         // remove the node from the list, so we could forback
+//         list.remove(list.size() - 1);
+//     }
+    
+    // 3.BFS
+    /*
+    We have to use a queue to do level order traversal, and using another queue to record every path.
+    The queue to record every path is too Space Consuming. So BFS is not a good solution here.
+    */
+}
