883d95eda15402f70e1712c8f7c17ad81da2880a
Jason Chen <chenjb90@gmail.com>
2019-04-29 23:13:25 -0700

    Create LeetCode-222-Count-Complete-Tree-Nodes.java

---
+++ src/LeetCode-222-Count-Complete-Tree-Nodes.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+}
