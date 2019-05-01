5b7842be4c875f231f31e7df24929bd6749db4de
Jason Chen <chenjb90@gmail.com>
2019-04-30 21:46:33 -0700

    Create LeetCode-255-Verify-Preorder-Sequence-in-Binary-Search-Tree.java

---
+++ src/LeetCode-255-Verify-Preorder-Sequence-in-Binary-Search-Tree.java
@@ -0,0 +1,47 @@
+/*
+
+preorder    root | left | right
+
+preorder[0] is root
+left < root < right
+
+*/
+
+class Solution {
+    
+    // 1.Divide & Conquer
+    // Space O(1)
+//     public boolean verifyPreorder(int[] preorder) {
+//         if (preorder == null || preorder.length <= 2) return true;
+        
+//         return verifyPreorder(preorder, 0, preorder.length - 1);
+//     }
+    
+//     private boolean verifyPreorder(int[] arr, int start, int end) {
+//         // We get convergenced.
+//         if (start >= end) return true;
+        
+//         int root = arr[start];
+//         // Verify left tree
+//         int i = start + 1;
+//         for (; i <= end; i++) {
+//             if (arr[i] == root) return false;
+//             if (arr[i] > root) break;
+//         }
+        
+//         // Verify right tree
+//         int j = i;
+//         for (; j <= end; j++) {
+//             if (arr[j] <= root) return false;
+//         }
+        
+//         // i == start + 1, means the left tree is null, so we only need to verify right tree
+//         if (i == start + 1) return verifyPreorder(arr, start + 1, end);
+        
+//         // we need verify both left tree and right tree.
+//         return verifyPreorder(arr, start + 1, i - 1) && verifyPreorder(arr, i, end);
+//     }
+    
+    // 2.DFS
+
+}
