4fe23e27f0045edd51cd8d8722731bec5b57a8bd
Jason Chen <chenjb90@gmail.com>
2019-05-09 14:14:49 -0700

    Update LeetCode-1-Two-Sum.java

--- src/LeetCode-1-Two-Sum.java
+++ src/LeetCode-1-Two-Sum.java
@@ -27,4 +27,42 @@ public class Solution {
         return result;
         
     }
+    
+    // 1.Wrong Answer, as we need return the indices, not the value. So I cannot sort the array.
+//     public int[] twoSum(int[] nums, int target) {
+//         Arrays.sort(nums);
+//         int[] result = new int[2];
+
+//         int l = 0, r = nums.length - 1;
+//         while (l < r) {
+//         	if (nums[l] + nums[r] == target) {
+//         		result[0] = l;
+//         		result[1] = r;
+//         		return result;
+//         	} else if (nums[l] + nums[r] < target) {
+//         		l++;
+//         	} else {
+//         		r--;
+//         	}
+//         }
+
+//         return result;
+//     }
+    
+    // 2.Correct Answer. Using a HashMap
+    public int[] twoSum(int[] nums, int target) {
+    	int[] result = new int[2];
+
+    	HashMap<Integer, Integer> map = new HashMap<>();
+    	for (int i = 0; i < nums.length; i++) {
+    		if (map.containsKey(nums[i])) {
+    			result[0] = map.get(nums[i]);
+    			result[1] = i;
+    			return result;
+    		}
+            map.put(target - nums[i], i);
+    	}
+    	return null;
+    }
+    
 }
