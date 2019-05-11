a9007fdb08b998da0bf6a1c9a8e6e9f71de1c4cc
Jason Chen <chenjb90@gmail.com>
2019-05-11 16:10:25 -0700

    Update LeetCode-33-Search-in-Rotated-Sorted-Array.java

--- src/LeetCode-33-Search-in-Rotated-Sorted-Array.java
+++ src/LeetCode-33-Search-in-Rotated-Sorted-Array.java
@@ -8,30 +8,42 @@ Analysis:
 
 */
 public class Solution {
-    // 1.Iterative(From JiuZhang)
-    // public int search(int[] nums, int target) {
-    //     if(nums == null || nums.length == 0) return -1;
-    //     if(nums.length == 1 && nums[0] == target) return 0;
+    // 1.Iterative(Similar to JiuZhang's solution)
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1; 
+        int lo = 0, hi = nums.length - 1;
         
-    //     int len = nums.length;
-    //     int lo = 0, hi = len - 1;
-    //     while(lo + 1 < hi){
-    //         int mid = lo + (hi - lo) / 2;
+        while (lo + 1 < hi) {
+            int mid = lo + (hi - lo) / 2;
             
-    //         if(nums[mid] == target) return mid;
-    //         if(nums[mid] > nums[lo]){
-    //             if(nums[lo] <= target && target < nums[mid]) hi = mid;
-    //             else lo = mid;
-    //         }else{
-    //             if(nums[mid] < target && target <= nums[hi]) lo = mid;
-    //             else hi = mid;
-    //         }
-    //     }
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            if (nums[mid] > nums[0]) {
+                // means mid is in the left side of pivot
+                if (nums[lo] <= target && target < nums[mid]) {
+                    // means target is between lo and mid
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                // means mid is in the right side of pivot
+                if (nums[mid] < target && target <= nums[hi]) {
+                    // means target is between mid and hi
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
         
-    //     if(nums[lo] == target) return lo;
-    //     if(nums[hi] == target) return hi;
-    //     return -1;
-    // }
+        if (nums[lo] == target) return lo;
+        if (nums[hi] == target) return hi;
+        
+        return -1;
+    }
     
     // 2.Iterative(From myself)
     // public int search(int[] nums, int target) {
