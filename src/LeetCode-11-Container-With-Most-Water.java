5efe802fe62eb760c376e7fcde3a91f575bd4c8a
Jason Chen <chenjb90@gmail.com>
2019-05-10 23:08:20 -0700

    Update LeetCode-11-Container-With-Most-Water.java

--- src/LeetCode-11-Container-With-Most-Water.java
+++ src/LeetCode-11-Container-With-Most-Water.java
@@ -6,16 +6,16 @@ ProgramCreek: http://www.programcreek.com/2014/03/leetcode-container-with-most-w
 */
 public class Solution {
     public int maxArea(int[] height) {
-        if(height == null || height.length <2) return 0;
-        
-        int result = 0, left = 0;
-        int right = height.length - 1;
-        
-        while(left < right){
-            result = Math.max(result, (right - left) * Math.min(height[left], height[right]));
-            if(height[left] < height[right]) left++;
-            else right--;
-        }
+        int result = Integer.MIN_VALUE, l = 0, r = height.length - 1;
+         while (l < r) {
+             result = Math.max(result, Math.min(height[l], height[r]) * (r - l));
+             // To get the possibility of having a bigger size, we have to move the smaller one
+             if (height[l] < height[r]) {
+                 l++;
+             } else {
+                 r--;
+             }
+         }
         return result;
     }
 }
