87c9385d3389ddcd14aba96c3afc84468071905c
Jason Chen <chenjb90@gmail.com>
2019-05-12 18:54:29 -0700

    Update LeetCode-56-Merge-Intervals.java

--- src/LeetCode-56-Merge-Intervals.java
+++ src/LeetCode-56-Merge-Intervals.java
@@ -58,36 +58,119 @@ public class Solution {
     }
 }
 
-// https://leetcode.com/problems/merge-intervals/discuss/21222/A-simple-Java-solution
 class Solution {
+    // 1.
+    /*
+    https://leetcode.com/problems/merge-intervals/discuss/21223/Beat-98-Java.-Sort-start-and-end-respectively
+    
+    Sort the start pointers and end pointers independently.
+    Scan the end pointer array. Whenever we found a end[j] < start[j + 1], means we found a new non-overlapping interval.
+    */
+//     public int[][] merge(int[][] intervals) {
+//         List<int[]> result = new ArrayList<>();
+//         if (intervals == null || intervals.length == 0) return result.toArray(new int[0][]);
+        
+//         int n = intervals.length;
+//         int[] start = new int[n];
+//         int[] end = new int[n];
+        
+//         for (int i = 0; i < n; i++) {
+//             start[i] = intervals[i][0];
+//             end[i] = intervals[i][1];
+//         }
+        
+//         Arrays.sort(start);
+//         Arrays.sort(end);
+        
+//         /*
+//         The idea here is keep on scaning the end.
+        
+//         When ever we found a end[j] < start[j + 1], that means we found an non-overlapping interval, add to result.
+        
+//         Be careful when we reached the last element of end, we have to add the last element to result.
+//         */
+//         // i is the index in start array, j is the index in end array.
+//         for (int i = 0, j = 0; j < n; j++) {
+//             if (j == n - 1 || start[j + 1] > end[j]) {
+//                 // means we found a new non-overlapping interval
+//                 result.add(new int[] {start[i], end[j]});
+//                 i = j + 1;  // after we add a new non-overlapping interval, we have to let the start pointer sync up with end pointer
+//             }
+//         }
+        
+//         return result.toArray(new int[result.size()][]);
+//     }
+    
+    // 2. A similar solution using two pointers.
+//     public int[][] merge(int[][] intervals) {
+//         List<int[]> result = new ArrayList<>();
+//         if (intervals == null || intervals.length == 0) return result.toArray(new int[0][]);
+        
+//         int n = intervals.length;
+//         int[] start = new int[n];
+//         int[] end = new int[n];
+        
+//         for (int i = 0; i < n; i++) {
+//             start[i] = intervals[i][0];
+//             end[i] = intervals[i][1];
+//         }
+        
+//         Arrays.sort(start);
+//         Arrays.sort(end);
+        
+//         /*
+//         The idea here is keep on scaning the end.
+        
+//         When ever we found a end[j] < start[j + 1], that means we found an non-overlapping interval, add to result.
+        
+//         Be careful when we reached the last element of end, we have to add the last element to result.
+//         */
+//         // i is the index in start array, j is the index in end array.
+//         int i = 0, j = 0;
+//         while (j < n) {
+//             if (j == n - 1 || start[j + 1] > end[j]) {
+//                 // means we found a new non-overlapping interval
+//                 result.add(new int[] {start[i], end[j]});
+//                 i = j + 1;  // after we add a new non-overlapping interval, we have to let the start pointer sync up with end pointer
+//             }
+//             j++;
+//         }
+        
+//         return result.toArray(new int[result.size()][]);
+//     }
+    
+    // 3. Sorting the whole intervals array.
     public int[][] merge(int[][] intervals) {
-        if (intervals == null || intervals.length <= 1) return intervals;
-        
         List<int[]> result = new ArrayList<>();
-        Arrays.sort(intervals, Comparator.comparing((int[] arr) -> arr[0]));
+        if (intervals == null || intervals.length == 0) return result.toArray(new int[0][]);
         
-        int start = intervals[0][0], end = intervals[0][1];
-        for (int i = 0; i < intervals.length; i++) {
-            int[] interval = intervals[i];
-            if (end >= interval[0]) {
-                // means the [start, end] and interval is overlapped, we could merge them
-                end = Math.max(end, interval[1]);
-            } else {
-                // means the [start, end] and interval is not overlapping, we could add the result to 
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int n = intervals.length;
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        
+        /*
+        The idea here is keep on scaning the intervals.
+        
+        When ever we found a end[j] < start[j + 1], that means we found an non-overlapping interval, add to result.
+        
+        Be careful when we reached the last element of end, we have to add the last element to result.
+        */
+        // i is the index in start array, j is the index in end array.
+        for (int[] interval : intervals) {
+            if (interval[0] > end) {
+                // means [start, end] is a new non-overlapping interval
                 result.add(new int[]{start, end});
                 start = interval[0];
                 end = interval[1];
+            } else {
+                // [start, end] and current interval is overlapping, we need update the end
+                end = Math.max(end, interval[1]);
             }
         }
-        
         result.add(new int[]{start, end});
         
-        // Convert List<int[]> result to int[][]
-        int[][] r = new int[result.size()][2];
-        for(int i = 0; i < result.size(); i++) {
-            r[i] = result.get(i);
-        }
-        
-        return r;
+        return result.toArray(new int[result.size()][]);
     }
+    
 }
