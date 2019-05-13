a960101c57bf5cece3385909653f42fed9577a63
Jason Chen <chenjb90@gmail.com>
2019-05-12 17:37:16 -0700

    Update LeetCode-54-Spiral-Matrix.java

--- src/LeetCode-54-Spiral-Matrix.java
+++ src/LeetCode-54-Spiral-Matrix.java
@@ -7,49 +7,48 @@ ProgramCreek: http://www.programcreek.com/2013/01/leetcode-spiral-matrix-java/
 Analysis:
 
 */
-class Solution {
+public class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<Integer>();
-        if (matrix == null || matrix.length == 0) return result;
+        if(matrix == null || matrix.length < 1) return result;
         
-        // xRange is the length that could move in rows, rRange is the length taht could move in columns
-        int xRange = matrix.length, yRange = matrix[0].length;
+        int m = matrix.length, n = matrix[0].length;
         
-        if (xRange == 1 && yRange == 1) {
+        if (m == 1 && n == 1) {
             result.add(matrix[0][0]);
             return result;
         }
         
-        // x, y is the indices
-        int x = 0, y = 0;
+        int x = 0, y = 0; // the start point
         
-        while (xRange > 0 && yRange > 0) {
-            if (xRange == 1) {
-                // which means there is only one row
-                for (int i = 0; i < yRange; i++) result.add(matrix[x][y++]);
-                break;
-            } else if (yRange == 1) {
-                // means there is only one column
-                for (int i = 0; i < xRange; i++) result.add(matrix[x++][y]);
-                break;
+        while (m > 0 && n > 0) {
+            // Consider how m == 1,n == 1 are handled.
+            if (m == 1) {
+                for (int i = 0; i < n; i++) result.add(matrix[x][y++]);
+                return result;
+            }
+            if (n == 1) {
+                for (int i = 0; i < m; i++) result.add(matrix[x++][y]);
+                return result;
             }
             
-            // Should be xRange - 1 here, as we are using "x++", so in next line, it will use x as colume number.
-            for (int i = 0; i < yRange - 1; i++) result.add(matrix[x][y++]);
+            // Be careful: consider carefully why n - 1 and m - 1 here.
+            for (int i = 0; i < n - 1; i++) result.add(matrix[x][y++]);
             
-            for (int i = 0; i < xRange - 1; i++) result.add(matrix[x++][y]);
+            for (int i = 0; i < m - 1; i++) result.add(matrix[x++][y]);
             
-            for (int i = 0; i < yRange - 1; i++) result.add(matrix[x][y--]);
+            for (int i = 0; i < n - 1; i++) result.add(matrix[x][y--]);
             
-            for (int i = 0; i < xRange - 1; i++) result.add(matrix[x--][y]);
+            for (int i = 0; i < m - 1; i++) result.add(matrix[x--][y]);
             
-            // Start point from [0, 0] to [1, 1]
+            m -= 2;
+            n -= 2;
+            
+            // finally x, y will come back to the original start point, so we need increment 
             x++;
             y++;
-            // One round will add the left most, and right most colume into result, so should be xRange - 2; the same thing for yRange
-            xRange -= 2;
-            yRange -= 2;
         }
+        
         return result;
     }
 }
