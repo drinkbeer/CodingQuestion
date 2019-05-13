1dba18d3cbdebdd9964cad8b7585784a32102745
Jason Chen <chenjb90@gmail.com>
2019-05-12 23:49:33 -0700

    Update LeetCode-66-Plus-One.java

--- src/LeetCode-66-Plus-One.java
+++ src/LeetCode-66-Plus-One.java
@@ -12,4 +12,29 @@ public class Solution {
         newDigits[0] = 1;
         return newDigits;
     }
+    
+    public int[] plusOne(int[] digits) {
+        
+        int carry = 1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            int digit = (digits[i] + carry) % 10;
+            carry = (digits[i] + carry) / 10;
+            digits[i] = digit;
+        }
+        
+//         if (carry > 0) {
+//             int[] arr = new int[digits.length + 1];
+//             arr[0] = carry;
+//             for (int i = 1; i < digits.length + 1; i++) {
+//                 arr[i] = digits[i - 1];
+//             }
+            
+//             return arr;
+//         }
+        if (carry == 0) return digits;
+        
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
 }
