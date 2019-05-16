f8401a80e655385d8adcb4e7aa4608221f9d4334
Jason Chen <chenjb90@gmail.com>
2019-05-16 16:28:35 -0700

    Update LeetCode-55-Jump-Game.java

--- src/LeetCode-55-Jump-Game.java
+++ src/LeetCode-55-Jump-Game.java
@@ -6,8 +6,11 @@ ProgramCreek: http://www.programcreek.com/2014/03/leetcode-jump-game-java/
 
 Analysis:
 1.DP will exceed time limit.
+
 2.DP looks like greedy
+
 3.Greedy also must optimize carefully.
+
 4.Greedy(better)
 Scan from 0 to nums.length-1
 Divide the scanning process into different phase -- [start, end]
@@ -18,80 +21,77 @@ public class Solution {
     
     // 1.Dynamic Programming
     // Time Limit Exceeded
-    // public boolean canJump(int[] nums) {
-    //     if(nums == null || nums.length == 0) return false;
-    //     if(nums.length <= 1) return true;
+    // Space O(N)
+    // Time O(N)
+//     public boolean canJump(int[] nums) {
+//         if(nums == null || nums.length == 0) return false;
+//         if(nums.length <= 1) return true;
         
-        // means if curr pace can be reached
-    //     boolean[] state = new boolean[nums.length];
-    //     state[0] = true;
+//         // means if curr pace can be reached
+//         boolean[] state = new boolean[nums.length];
+//         state[0] = true;
         
-    //     for(int i = 1; i < nums.length; i++){
-    //         for(int j = 0; j < i; j++){
-    //             if(state[j] && j + nums[j] >= i) {
-    //                 state[i] = true;
-    //                 break;       //optimize: here we must break as soon as we can jump from this j to i
-    //             }
-    //         }
-    //     }
-        
-    //     return state[nums.length - 1];
-        
-    // }
+//         for(int i = 1; i < nums.length; i++){
+//             for (int j = 0; j <= i; j++) {
+//                 if (state[j] && j + nums[j] >= i) {
+//                     state[i] = true;
+//                     break;    //optimize: here we must break as soon as we can jump from this j to i
+//                 }
+//             }
+//         }
     
-    // 2.Dynamic Programming
-    // public boolean canJump(int[] nums) {
-    //     if (nums == null || nums.length == 0) return false;
-    //     if (nums.length == 1) return true;
+    // 2.DP
+//     public boolean canJump(int[] nums) {
+//         if(nums == null || nums.length == 0) return false;
+//         if(nums.length <= 1) return true;
         
-    //     // state: means the max pace can be covered
-    //     int[] state = new int[nums.length];
-    //     // init state
-    //     state[0] = nums[0];
+//         // state: means the max pace can be covered
+//         int[] state = new int[nums.length];
+//         state[0] = nums[0];
         
-    //     for(int i = 1; i < nums.length; i++){
-    //         if(i > state[i - 1]) return false;
-            
-    //         state[i] = Math.max(state[i - 1], i + nums[i]);
-            
-    //         if(state[i] >= nums.length - 1) return true;
-    //     }
+//         for (int i = 1; i < nums.length; i++) {
+//             if (i > state[i - 1]) return false;
+//             state[i] = Math.max(state[i - 1], i + nums[i]);
+//             if (state[i] >= nums.length - 1) return true;
+//         }
         
-    //     return true;
-    // }
+//         return state[nums.length - 1] >= nums.length - 1;
+//     }
     
     // 3.Greedy
-    // public boolean canJump(int[] nums) {
-    //     if(nums == null || nums.length == 0) return false;
-    //     if(nums.length == 1) return true;
+//     public boolean canJump(int[] nums) {
+//         if(nums == null || nums.length == 0) return false;
+//         if(nums.length <= 1) return true;
         
-    //     int max = nums[0];
+//         int max = nums[0];
         
-    //     for(int i = 1; i < nums.length; i++){
-    //         if(max < i) return false;
-            
-    //         max = Math.max(max, i + nums[i]);
-            
-    //         if(max >= nums.length - 1) return true;
-    //     }
-    //     return true;
-    // }
-    
-    // 4.Greedy
+//         for (int i = 1; i < nums.length; i++) {
+//             if (i > max) return false;
+//             max = Math.max(max, i + nums[i]);
+//             if (max >= nums.length - 1) return true;
+//         }
+        
+//         return max >= nums.length - 1;
+//     }
+
+    // 4. Greedy
+    // Time O(N)
+    // Space O(1)
+    // This one is super faster than the third solution, as it skipped a lot of calculation.
     public boolean canJump(int[] nums) {
         if(nums == null || nums.length == 0) return false;
+        if(nums.length <= 1) return true;
         
         int start = 0, end = 0;
-        
-        while(end < nums.length - 1){
-            int farthest = 0;
-            for(int i = start; i <= end; i++){
-                farthest = Math.max(farthest, i + nums[i]);
+        while (end < nums.length) {
+            int fastest = 0;
+            for (int i = start; i <= end; i++) {
+                fastest = Math.max(fastest, i + nums[i]);
             }
-            if(farthest <= end) return false;
-            if(farthest >= nums.length - 1) return true;
-            start = end + 1; 
-            end = farthest;
+            if (fastest <= end) return false; // means no step forward, we are blocking in current run
+            if (fastest >= nums.length - 1) return true;
+            start = end + 1;
+            end = fastest;
         }
         
         return true;
