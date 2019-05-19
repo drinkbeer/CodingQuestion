f0387bdac8ea6f7b157da83e5efac33d4e0a71d7
Jason Chen <chenjb90@gmail.com>
2019-05-19 15:34:26 -0700

    Update LeetCode-119-Pascals-Triangle-II.java

--- src/LeetCode-119-Pascals-Triangle-II.java
+++ src/LeetCode-119-Pascals-Triangle-II.java
@@ -28,4 +28,22 @@ public class Solution {
         
         return result;
     }
-}
+    
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new ArrayList<>();
+        
+        result.add(1);
+        
+        for (int i = 1; i <= rowIndex; i++) {
+            List<Integer> curr = new ArrayList<>();
+            
+            curr.add(1);
+            for (int j = 0; j < i - 1; j++) {
+                curr.add(result.get(j) + result.get(j + 1));
+            }
+            curr.add(1);
+            result = curr;
+        }
+        
+        return result;
+    }}
