9a82632aab81a7ab76b96b44f391dd68acc558ed
Jason Chen <chenjb90@gmail.com>
2019-05-19 14:06:41 -0700

    Update LeetCode-79-Word-Search.java

--- src/LeetCode-79-Word-Search.java
+++ src/LeetCode-79-Word-Search.java
@@ -9,6 +9,7 @@ Analysis:
 DFS
 */
 public class Solution {
+    // 1. DFS
     public boolean exist(char[][] board, String word) {
         boolean result = false;
         int m = board.length;
@@ -49,4 +50,46 @@ public class Solution {
         
         return false;
     }
+    
+   // 2. DFS
+   public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || word.length() == 0) return false;
+        
+        int n = board.length, m = board[0].length;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (exist(board, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean exist(char[][] board, int i, int j, String word, int start) {
+        int n = board.length, m = board[0].length;
+        
+        // end condition of DFS
+        if (i < 0 || j < 0 || i >= n || j >= m) return false;
+        if (board[i][j] != word.charAt(start)) return false;
+        
+        //// means the board[i][j] is equals to word.charAt(start)
+        
+        // success criteria
+        if (start == word.length() - 1) return true;
+        
+        //// to avoid to search back, for instance: ["aa"], "aaa" ==> should be false, this is the most tricky part of this problem
+        char temp = board[i][j];
+        board[i][j] = '#';
+        
+        if(exist(board, i + 1, j, word, start + 1) || exist(board, i, j + 1, word, start + 1) || exist(board, i - 1, j, word, start + 1) || exist(board, i, j - 1, word, start + 1)) {
+            return true;
+        }
+        
+        board[i][j] = temp;
+        
+        return false;
+    }    
 }
