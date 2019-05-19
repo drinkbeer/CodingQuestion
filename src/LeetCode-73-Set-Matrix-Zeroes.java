40cb1c5557ec77d181d9ac6649d09ccb46f1a3ab
Jason Chen <chenjb90@gmail.com>
2019-05-18 17:35:29 -0700

    Update LeetCode-73-Set-Matrix-Zeroes.java

--- src/LeetCode-73-Set-Matrix-Zeroes.java
+++ src/LeetCode-73-Set-Matrix-Zeroes.java
@@ -8,6 +8,7 @@ Analysis:
 
 */
 public class Solution {
+    // 1.
     public void setZeroes(int[][] matrix) {
         boolean firstRowZeroes = false;
         boolean firstColumnZeroes = false;
@@ -59,5 +60,38 @@ public class Solution {
             }
         }
         
+    }
+    
+   // 2.
+   public void setZeroes(int[][] matrix) {
+        boolean fr = false,fc = false;
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0) {
+                    if(i == 0) fr = true;
+                    if(j == 0) fc = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < matrix.length; i++) {
+            for(int j = 1; j < matrix[0].length; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(fr) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if(fc) {
+            for(int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
     }
 }
