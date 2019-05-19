b2fb88b3cec2e4981b40cb3d6e648afeb7f5276c
Jason Chen <chenjb90@gmail.com>
2019-05-19 14:53:52 -0700

    Update LeetCode-81-Search-in-Rotated-Sorted-Array-II.java

--- src/LeetCode-81-Search-in-Rotated-Sorted-Array-II.java
+++ src/LeetCode-81-Search-in-Rotated-Sorted-Array-II.java
@@ -8,15 +8,6 @@ Analysis:
 
 */
 public class Solution {
-    // public boolean search(int[] nums, int target) {
-    //     if(nums == null || nums.length == 0) return false;
-        
-    //     for(int i = 0; i < nums.length; i++){
-    //         if(nums[i] == target) return true;
-    //     }
-        
-    //     return false;
-    // }
     
     public boolean search(int[] nums, int target) {
         int left=0;
@@ -44,6 +35,43 @@ public class Solution {
             }    
         }
      
+        return false;
+    }
+    
+    // https://leetcode.com/problems/search-in-rotated-sorted-array-ii/discuss/28216/Java-1ms-binary-search-solution-with-comments
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return false;
+        
+        int lo = 0, hi = nums.length - 1;
+        while (lo + 1 < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) return true;
+            
+            if (nums[mid] > nums[lo]) {
+                // which means mid is in the left side of pivot
+                if (nums[lo] <= target && target < nums[mid]) {
+                    // target is between [lo, mid)
+                    hi = mid - 1;   
+                } else {
+                    lo = mid + 1;
+                }
+            } else if (nums[mid] < nums[lo]) {
+                // which means mid is in the right side of pivot
+                if (nums[mid] < target && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            } else {
+                // when nums[lo] == nums[mid], we just move the lo pointer to right
+                // // have no idea about the array, but we can exclude nums[start] because nums[start] == nums[mid]
+                lo++;
+            }
+        }
+        
+        if (nums[lo] == target) return true;
+        if (nums[hi] == target) return true;
+        
         return false;
     }
 }
