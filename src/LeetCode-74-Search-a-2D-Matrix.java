bf396e650ee18ca6dc7a1978443283f9ffb8604f
Jason Chen <chenjb90@gmail.com>
2019-05-18 22:48:08 -0700

    Update LeetCode-74-Search-a-2D-Matrix.java

--- src/LeetCode-74-Search-a-2D-Matrix.java
+++ src/LeetCode-74-Search-a-2D-Matrix.java
@@ -36,4 +36,32 @@ public class Solution {
         
         return false;
     }
+    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+        int n = matrix.length, m = matrix[0].length;
+        int lo = 0, hi = m * n - 1;
+        
+        while (lo + 1 < hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            int i = mid / m;
+            int j = mid % m;
+            
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            
+            if (matrix[i][j] > target) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        
+        if (matrix[lo / m][lo % m] == target) return true;
+        if (matrix[hi / m][hi % m] == target) return true;
+        return false;
+    }
+    
 }
