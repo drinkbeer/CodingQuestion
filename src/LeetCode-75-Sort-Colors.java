746c06c25acd478df6a677044d4d8556d5f07d28
Jason Chen <chenjb90@gmail.com>
2019-05-19 13:36:19 -0700

    Update LeetCode-75-Sort-Colors.java

--- src/LeetCode-75-Sort-Colors.java
+++ src/LeetCode-75-Sort-Colors.java
@@ -8,6 +8,23 @@ Analysis:
 Use two pointers, one is the 0 area's right bound+1, another is 1 area's left bound-1.
 */
 public class Solution {
+    
+    // 1. Counting Sort Time O(N)
+    public void sortColors(int[] nums) {
+        int countZero = 0, countOne = 0, countTwo = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) countZero++;
+            if (nums[i] == 1) countOne++;
+            if (nums[i] == 2) countTwo++;
+        }
+        
+        for (int i = 0; i < countZero; i++) nums[i] = 0;
+        for (int i = 0; i < countOne; i++) nums[i + countZero] = 1;
+        for (int i = 0; i < countTwo; i++) nums[i + countZero + countOne] = 2;
+    }
+    
+    // 2. Two Pointers
     public void sortColors(int[] nums) {
         if(nums == null || nums.length <= 1) return;
         // left is the 0's right bound+1, right is 1's left bound-1
