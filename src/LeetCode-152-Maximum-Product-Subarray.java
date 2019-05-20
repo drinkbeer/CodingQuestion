b4c82f20124778973eded088306235526b72c01b
Jason Chen <chenjb90@gmail.com>
2019-05-19 23:32:40 -0700

    Update LeetCode-152-Maximum-Product-Subarray.java

--- src/LeetCode-152-Maximum-Product-Subarray.java
+++ src/LeetCode-152-Maximum-Product-Subarray.java
@@ -23,21 +23,47 @@ public class Solution {
     Inspired by: https://leetcode.com/problems/maximum-product-subarray/discuss/48230/Possibly-simplest-solution-with-O(n)-time-complexity
     
     */
-    public int maxProduct(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
+//     public int maxProduct(int[] nums) {
+//         if (nums == null || nums.length == 0) return 0;
 
-        int min = nums[0], max = nums[0], result = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < 0) {
-                int temp = max;
-                max = min;
-                min = temp;
-            }
-            max = Math.max(max * nums[i], nums[i]);
-            min = Math.min(min * nums[i], nums[i]);
-            result = Math.max(result, max);
+//         int min = nums[0], max = nums[0], result = nums[0];
+//         for (int i = 1; i < nums.length; i++) {
+//             if (nums[i] < 0) {
+//                 int temp = max;
+//                 max = min;
+//                 min = temp;
+//             }
+//             max = Math.max(max * nums[i], nums[i]);
+//             min = Math.min(min * nums[i], nums[i]);
+//             result = Math.max(result, max);
+//         }
+//         return result;
+//     }
+    
+    // 3.
+    /*
+    https://leetcode.com/problems/maximum-product-subarray/discuss/48404/Accepted-Java-solution
+    */
+    public int maxProduct(int[] a) {
+      if (a == null || a.length == 0)
+        return 0;
+
+      int ans = a[0], min = ans, max = ans;
+
+      for (int i = 1; i < a.length; i++) {
+        if (a[i] >= 0) {
+          max = Math.max(a[i], max * a[i]);
+          min = Math.min(a[i], min * a[i]);
+        } else {
+          int tmp = max;
+          max = Math.max(a[i], min * a[i]);
+          min = Math.min(a[i], tmp * a[i]);
         }
-        return result;
+
+        ans = Math.max(ans, max);
+      }
+
+      return ans;
     }
 
 }
