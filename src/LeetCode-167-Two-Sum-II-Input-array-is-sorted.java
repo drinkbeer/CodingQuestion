fcc069393a6dfbb00601671954934709f3def597
Jason Chen <chenjb90@gmail.com>
2019-05-20 01:56:17 -0700

    Update LeetCode-167-Two-Sum-II-Input-array-is-sorted.java

--- src/LeetCode-167-Two-Sum-II-Input-array-is-sorted.java
+++ src/LeetCode-167-Two-Sum-II-Input-array-is-sorted.java
@@ -7,6 +7,20 @@ ProgramCreek: http://www.programcreek.com/2014/03/two-sum-ii-input-array-is-sort
 Analysis: 
 */
 public class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+        
+//         for (int i = 0; i < nums.length; i++) {
+//             if (map.containsKey(nums[i])) {
+//                 return new int[] {map.get(nums[i]) + 1, i + 1};
+//             } else {
+//                 map.put(target - nums[i], i);
+//             }
+//         }
+        
+//         return new int[2];
+//     }
+    
     public int[] twoSum(int[] numbers, int target) {
         int[] result = new int[2];
         if(numbers == null || numbers.length <2) return result;
