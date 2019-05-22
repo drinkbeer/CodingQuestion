942cd39e056d44d808061d438569c3a68d0ede36
Jason Chen <chenjb90@gmail.com>
2019-05-22 00:42:01 -0700

    Update LeetCode-3-Longest-Substring-Without-Repeating-Characters.java

--- src/LeetCode-3-Longest-Substring-Without-Repeating-Characters.java
+++ src/LeetCode-3-Longest-Substring-Without-Repeating-Characters.java
@@ -10,7 +10,7 @@ Analysis:
 Use two pointers.
 */
 public class Solution {
-    // 1.Two Pointers
+    // 1.Two Pointers, using a int[] as mask.
     // public int lengthOfLongestSubstring(String s) {
         
     //     int[] map = new int[256];   // map from character's ASCII to if it's visited
@@ -32,7 +32,25 @@ public class Solution {
     //     return Math.max(result, s.length() - slow);
     // }
     
-    // 2. Two Pointers
+    // 2. Two Pointers, similar to JiuZhang's algorithm, using a int[] as mask.
+    public int lengthOfLongestSubstring(String s) {
+        int[] map = new int[256];   // map from character's ASCII to if it's visited        
+        int result = Integer.MIN_VALUE;
+        
+        int slow = 0, fast = 0;
+        for(; slow < s.length(); slow++){
+            while (fast < s.length() && map[s.charAt(fast)] == 0) {
+                map[s.charAt(fast)] = 1; 
+                result = Math.max(result, fast - slow + 1);
+                fast++;
+            }
+            map[s.charAt(slow)] = 0;
+        }
+        
+        return result == Integer.MIN_VALUE ? 0 : result;
+    }
+    
+    // 3. Two Pointers
     public int lengthOfLongestSubstring(String s) {
         if(s == null || s.length() == 0) return 0;
         
@@ -56,7 +74,7 @@ public class Solution {
         return result;
     }
     
-    // 3. Two Pointers
+    // 4. Two Pointers, using a Set as Mask.
     public int lengthOfLongestSubstring(String s) {
         Set<Character> set = new HashSet<>();
         
