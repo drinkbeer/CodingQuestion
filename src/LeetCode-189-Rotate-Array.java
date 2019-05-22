6177bcd6b26c2f04c80f6e78eaa0ef6369f4abc8
Jason Chen <chenjb90@gmail.com>
2019-05-21 21:26:37 -0700

    Update LeetCode-189-Rotate-Array.java

--- src/LeetCode-189-Rotate-Array.java
+++ src/LeetCode-189-Rotate-Array.java
@@ -4,25 +4,53 @@ LintCode: http://www.lintcode.com/problem/rotate-array/
 JiuZhang: http://www.jiuzhang.com/solutions/rotate-array/
 ProgramCreek: http://www.programcreek.com/2015/03/rotate-array-in-java/
 
+
+
+https://leetcode.com/problems/rotate-array/discuss/54250/Easy-to-read-Java-solution
+
+
+
 */
 public class Solution {
-    public void rotate(int[] nums, int k) {
-        if(nums == null || nums.length <= 1 || k < 1)return;
+//     public void rotate(int[] nums, int k) {
+//         if(nums == null || nums.length <= 1 || k < 1)return;
         
-        k = k % nums.length;    // must ensure k <= length
+//         k = k % nums.length;    // must ensure k <= length
         
-        reverse(nums, 0, nums.length - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
-    }
+//         reverse(nums, 0, nums.length - 1);
+//         reverse(nums, 0, k - 1);
+//         reverse(nums, k, nums.length - 1);
+//     }
     
-    private void reverse(int[] nums, int lo, int hi){
-        while(lo < hi){
-            int temp = nums[lo];
-            nums[lo] = nums[hi];
-            nums[hi] = temp;
-            lo++;
-            hi--;
+//     private void reverse(int[] nums, int lo, int hi){
+//         while(lo < hi){
+//             int temp = nums[lo];
+//             nums[lo] = nums[hi];
+//             nums[hi] = temp;
+//             lo++;
+//             hi--;
+//         }
+//     }
+    
+    /*
+    https://leetcode.com/problems/rotate-array/discuss/54289/My-three-way-to-solve-this-problem-the-first-way-is-interesting(JAVA)
+    */
+    public void rotate(int[] nums, int k) {
+        if(nums.length <= 1){
+            return;
         }
+        //step each time to move
+        int step = k % nums.length;
+        int[] tmp = new int[step];
+        for(int i = 0; i < step; i++){
+            tmp[i] = nums[nums.length - step + i];
+        }
+        for(int i = nums.length - step - 1; i >= 0; i--){
+            nums[i + step] = nums[i];
+        }
+        for(int i = 0; i < step; i++){
+            nums[i] = tmp[i];
+        }
+        
     }
 }
