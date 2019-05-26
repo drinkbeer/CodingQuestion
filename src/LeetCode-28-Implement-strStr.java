0bf7401ed0ef240ce75cdef5ae13955c32daabdf
Jason Chen <chenjb90@gmail.com>
2019-05-26 13:32:57 -0700

    Update LeetCode-28-Implement-strStr.java

--- src/LeetCode-28-Implement-strStr.java
+++ src/LeetCode-28-Implement-strStr.java
@@ -1,17 +1,37 @@
-class Solution {
+/*
+LeetCode: https://leetcode.com/problems/implement-strstr/
+LintCode: http://www.lintcode.com/problem/implement-strstr/
+JiuZhang: http://www.jiuzhang.com/solutions/implement-strstr/
+ProgramCreek: http://www.programcreek.com/2012/12/leetcode-implement-strstr-java/
+
+Analysis: 
+
+*/
+public class Solution {
+    // 1.Two Pointers
+    // public int strStr(String haystack, String needle) {
+    //     if(haystack == null || needle == null ) return 0;
+    //     if(needle.length() == 0) return 0;
+        
+    //     for(int i = 0; i <=  haystack.length() - needle.length(); i++){
+    //         int m = i;
+    //         int j = 0;
+    //         for(; j < needle.length(); j++){
+    //             if(haystack.charAt(m) != needle.charAt(j)) break;
+    //             m++;
+    //         }
+    //         if(j == needle.length()) return i;
+    //     }
+    //     return -1;
+    // }
+    
+    // 2.Directly use String.indexOf(String). Perhaps we are not allowed to use it in interview.
     public int strStr(String haystack, String needle) {
+        if(haystack == null || needle == null ) return 0;
+        if(needle.length() == 0) return 0;
         
-        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
-            int j = 0;
-            for (; j < needle.length(); j++) {
-                if (haystack.charAt(i + j) != needle.charAt(j)) {
-                    break;
-                }
-                
-            }
-            if (j == needle.length()) return i;
-        }
-        
-        return -1;
+        return haystack.indexOf(needle);
     }
+    
+    
 }
