bf818f77fad2f4ed2bfe72330097ca14264c923b
Jason Chen <chenjb90@gmail.com>
2019-05-26 23:59:51 -0700

    Update LeetCode-71-Simplify-Path.java

--- src/LeetCode-71-Simplify-Path.java
+++ src/LeetCode-71-Simplify-Path.java
@@ -62,4 +62,18 @@ public class Solution {
         return result.length() == 0 ? "/" : result;
     }
     
+    // 3.
+    // https://leetcode.com/problems/simplify-path/discuss/25686/Java-10-lines-solution-with-stack
+    public String simplifyPath(String path) {
+        Deque<String> stack = new LinkedList<>();
+        Set<String> skip = new HashSet<>(Arrays.asList("..",".",""));
+        for (String dir : path.split("/")) {
+            if (dir.equals("..") && !stack.isEmpty()) stack.pop();
+            else if (!skip.contains(dir)) stack.push(dir);
+        }
+        String res = "";
+        for (String dir : stack) res = "/" + dir + res;
+        return res.isEmpty() ? "/" : res;
+    }
+    
 }
