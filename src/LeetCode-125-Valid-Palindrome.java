515797cfa8a7c1098100236a21838da5276dfdef
Jason Chen <chenjb90@gmail.com>
2019-05-30 17:26:08 -0700

    Update LeetCode-125-Valid-Palindrome.java

--- src/LeetCode-125-Valid-Palindrome.java
+++ src/LeetCode-125-Valid-Palindrome.java
@@ -9,6 +9,7 @@ Analysis:
 
 */
 public class Solution {
+    // 1. Two Pointers
     public boolean isPalindrome(String s) {
         if(s == null || s.length() == 0) return true;
         
@@ -26,4 +27,32 @@ public class Solution {
         }
         return true;
     }
+    
+    // 2. Two Pointers
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) return true;
+        
+        int lo = 0, hi = s.length() - 1;
+        while (lo < hi) {
+            if (!Character.isLetterOrDigit(s.charAt(lo))) {
+                lo++;
+            } else if (!Character.isLetterOrDigit(s.charAt(hi))) {
+                hi--;
+            } else {
+                if (Character.toLowerCase(s.charAt(lo)) != Character.toLowerCase(s.charAt(hi))) return false;
+                lo++;
+                hi--;
+            }
+        }
+        
+        return true;
+    }
+    
+    // 3.Reverse and Compare
+    // https://leetcode.com/problems/valid-palindrome/discuss/39981/My-three-line-java-solution
+    // public boolean isPalindrome(String s) {
+    //     String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+    //     String rev = new StringBuffer(actual).reverse().toString();
+    //     return actual.equals(rev);
+    // }
 }
