e873e8a6ca7b1d47519e7f7316a1ba519cf259ef
Jason Chen <chenjb90@gmail.com>
2019-06-01 21:30:34 -0700

    Update LeetCode-93-Restore-IP-Addresses.java

--- src/LeetCode-93-Restore-IP-Addresses.java
+++ src/LeetCode-93-Restore-IP-Addresses.java
@@ -8,12 +8,45 @@ Other: http://www.cnblogs.com/yuzhangcmu/p/4106686.html
 Analysis: 
 
 */
-public class Solution {
+class Solution {
+    
+    // 1.String operation
+    /**
+    Runtime: 2 ms, faster than 87.03% of Java online submissions for Restore IP Addresses.
+    */
+//     public List<String> restoreIpAddresses(String s) {
+//         List<String> result = new ArrayList<>();
+//         if (s == null || s.length() < 4 || s.length() > 12) return result;
+//         int len = s.length();
+//         for (int i = 1; i < 4; i++) {
+//             for (int j = i + 1; j < i + 4; j++) {
+//                 for (int k = j + 1; k < j + 4 && k < len; k++) {
+//                     String s1 = s.substring(0, i), s2 = s.substring(i, j), s3 = s.substring(j, k), s4 = s.substring(k, len);
+//                     if (isValid(s1) && isValid(s2) && isValid(s3) && isValid(s4)) {
+//                         result.add(s1 + "." + s2 + "." + s3 + "." + s4);
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return result;
+//     }
+    
+//     private boolean isValid(String str) {
+//         if (str.length() > 1 && (str.charAt(0) == '0' || Integer.parseInt(str) > 255)) return false;
+//         return true;
+//     }
+    
+    // 2.DFS
+    /*
+    Runtime: 2 ms, faster than 87.03% of Java online submissions for Restore IP Addresses.
+
+    */
     public List<String> restoreIpAddresses(String s) {
         List<String> result = new ArrayList<String>();
         if(s == null || s.length() < 4 || s.length() > 12) return result;
         
-        List<String> list = new ArrayList<String>();
+        List<String> list = new ArrayList<>();
         DFS(s, 0, result, list);
         return result;
     }
@@ -47,4 +80,5 @@ public class Solution {
         int digit = Integer.valueOf(s);
         return digit >= 0 && digit <= 255;
     }
+    
 }
