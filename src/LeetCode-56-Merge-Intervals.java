1edabff50378468d33846f33d2a4de3b105352fc
Jason Chen <chenjb90@gmail.com>
2019-06-11 14:39:32 -0700

    Update LeetCode-56-Merge-Intervals.java

--- src/LeetCode-56-Merge-Intervals.java
+++ src/LeetCode-56-Merge-Intervals.java
@@ -1,63 +1,3 @@
-/*
-LeetCode:  https://leetcode.com/problems/merge-intervals/
-LintCode:  http://www.lintcode.com/problem/merge-intervals/
-JiuZhang:  http://www.jiuzhang.com/solutions/merge-intervals/
-ProgramCreek:  http://www.programcreek.com/2012/12/leetcode-merge-intervals/
-
-Analysis:  
-If intervals are sorted by start? No, you need to write a comparator to sort by yourself.
-
-*/
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
-public class Solution {
-    public List<Interval> merge(List<Interval> intervals) {
-        if(intervals == null || intervals.size() == 0) return intervals;
-        List<Interval> result = new ArrayList<Interval>();
-        
-        java.util.Collections.sort(intervals, new IntervalComparator());
-        Interval prev = intervals.get(0);
-        for(int i = 1; i < intervals.size(); i++){
-            Interval curr = intervals.get(i);
-            // if(curr.start <= prev.end){
-            //     //merge
-            //     // Be careful: [[1,4],[2,3]]    ->  [[1,4]] not [[1,3]]
-            //     Interval merge = new Interval(prev.start, Math.max(prev.end, curr.end));
-            //     prev = merge;
-            // }else{
-            //     //no need merge
-            //     result.add(prev);
-            //     prev = curr;
-            // }
-            
-            if(curr.start <= prev.end){
-                //modify prev's end
-                prev.end = Math.max(prev.end, curr.end);
-            }else{
-                //merge
-                result.add(prev);
-                prev = curr;
-            }
-        }
-        result.add(prev);
-        
-        return result;
-    }
-    
-    private class IntervalComparator implements Comparator<Interval>{
-        public int compare(Interval p, Interval q){
-            return p.start - q.start;
-        }
-    }
-}
-
 class Solution {
     // 1.
     /*
@@ -140,37 +80,97 @@ class Solution {
 //     }
     
     // 3. Sorting the whole intervals array.
-    public int[][] merge(int[][] intervals) {
-        List<int[]> result = new ArrayList<>();
-        if (intervals == null || intervals.length == 0) return result.toArray(new int[0][]);
+//     public int[][] merge(int[][] intervals) {
+//         List<int[]> result = new ArrayList<>();
+//         if (intervals == null || intervals.length == 0) return result.toArray(new int[0][]);
         
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        int n = intervals.length;
-        int start = intervals[0][0];
-        int end = intervals[0][1];
+//         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+//         int n = intervals.length;
+//         int start = intervals[0][0];
+//         int end = intervals[0][1];
         
-        /*
-        The idea here is keep on scaning the intervals.
+//         /*
+//         The idea here is keep on scaning the intervals.
         
-        When ever we found a end[j] < start[j + 1], that means we found an non-overlapping interval, add to result.
+//         When ever we found a end[j] < start[j + 1], that means we found an non-overlapping interval, add to result.
         
-        Be careful when we reached the last element of end, we have to add the last element to result.
-        */
-        // i is the index in start array, j is the index in end array.
-        for (int[] interval : intervals) {
-            if (interval[0] > end) {
-                // means [start, end] is a new non-overlapping interval
-                result.add(new int[]{start, end});
-                start = interval[0];
-                end = interval[1];
-            } else {
-                // [start, end] and current interval is overlapping, we need update the end
-                end = Math.max(end, interval[1]);
-            }
-        }
-        result.add(new int[]{start, end});
+//         Be careful when we reached the last element of end, we have to add the last element to result.
+//         */
+//         // i is the index in start array, j is the index in end array.
+//         for (int[] interval : intervals) {
+//             if (interval[0] > end) {
+//                 // means [start, end] is a new non-overlapping interval
+//                 result.add(new int[]{start, end});
+//                 start = interval[0];
+//                 end = interval[1];
+//             } else {
+//                 // [start, end] and current interval is overlapping, we need update the end
+//                 end = Math.max(end, interval[1]);
+//             }
+//         }
+//         result.add(new int[]{start, end});
         
-        return result.toArray(new int[result.size()][]);
-    }
+//         return result.toArray(new int[result.size()][]);
+//     }
     
+//     public int[][] merge(int[][] intervals) {
+//      if (intervals == null || intervals.length <= 1) return intervals;
+//         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+//         int start = intervals[0][0];
+//         int end = intervals[0][1];
+//         List<int[]> res = new ArrayList<>();
+
+//         for (int[] interval : intervals) {
+//             if (interval[0] <= end) {
+//                 end = Math.max(end, interval[1]);
+//             } else {
+//                 res.add(new int[]{start, end});
+//                 start = interval[0];
+//                 end = interval[1];
+//             }
+//         }
+//         res.add(new int[]{start, end});
+
+//         int[][] ans = new int[res.size()][2];
+
+//         for (int i = 0; i < res.size(); i++) {
+//             ans[i][0] = res.get(i)[0];
+//             ans[i][1] = res.get(i)[1];
+//         }
+
+//         return ans;
+//     }
+    
+    // Sorting the start array and end array.
+    /*
+    Runtime: 2 ms, faster than 99.79% of Java online submissions for Merge Intervals.
+    */
+     public int[][] merge(int[][] intervals) {
+         if (intervals == null || intervals.length == 0 || intervals.length == 1) return intervals;
+         
+         int[] start = new int[intervals.length];
+         int[] end = new int[intervals.length];
+         for (int i = 0; i < intervals.length; i++) {
+             start[i] = intervals[i][0];
+             end[i] = intervals[i][1];
+         }
+         Arrays.sort(start);
+         Arrays.sort(end);
+         
+         List<int[]> result = new ArrayList<>();
+         int lo = start[0], hi = end[0];
+         for (int i = 1; i < intervals.length; i++) {
+             if (hi >= start[i]) {
+                 // means we need merging
+                 hi = end[i];
+             } else {
+                 result.add(new int[]{lo, hi});
+                 lo = start[i];
+                 hi = end[i];
+             }
+         }
+         
+         result.add(new int[]{lo, hi});
+         return result.toArray(new int[result.size()][]);
+     }
 }
