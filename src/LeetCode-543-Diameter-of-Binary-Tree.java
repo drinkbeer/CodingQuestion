62eebbca1e2f4f505e2a57542bca146cc2130960
Jason Chen <chenjb90@gmail.com>
2019-06-28 13:28:56 -0700

    Update LeetCode-543-Diameter-of-Binary-Tree.java

--- src/LeetCode-543-Diameter-of-Binary-Tree.java
+++ src/LeetCode-543-Diameter-of-Binary-Tree.java
@@ -8,6 +8,7 @@
  * }
  */
 class Solution {
+    // 1.
     // longest path = max depth of left child tree + max depth of right child tree + 1
     public int diameterOfBinaryTree(TreeNode root) {
         if (root == null) return 0;
@@ -27,6 +28,27 @@ class Solution {
         
         // update max
         max[0] = Math.max(max[0], left + right + 1);
+        
+        return Math.max(left, right) + 1;
+    }
+    
+    // 2.
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
+        
+        int[] max = new int[] {Integer.MIN_VALUE};
+        maxDepth(root, max);
+        return max[0];
+    }
+    
+    // The depth is also the number of edges 
+    public int maxDepth(TreeNode root, int[] max) {
+        if (root == null) return 0;
+        
+        int left = maxDepth(root.left, max);
+        int right = maxDepth(root.right, max);
+        
+        max[0] = Math.max(max[0], left + right);
         
         return Math.max(left, right) + 1;
     }
