e3ab7e4aadfff5a1ee7f10fc292b760c53708d99
Jason Chen <chenjb90@gmail.com>
2019-07-01 22:23:14 -0700

    Update LeetCode-141-Linked-List-Cycle.java

--- src/LeetCode-141-Linked-List-Cycle.java
+++ src/LeetCode-141-Linked-List-Cycle.java
@@ -7,6 +7,10 @@ ProgramCreek:  http://www.programcreek.com/2012/12/leetcode-linked-list-cycle/
 Analysis:  
 Fast-slow node
 
+
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Linked List Cycle.
+Memory Usage: 36.8 MB, less than 99.99% of Java online submissions for Linked List Cycle.
+
 */
 /**
  * Definition for singly-linked list.
@@ -20,16 +24,19 @@ Fast-slow node
  * }
  */
 public class Solution {
+    
     public boolean hasCycle(ListNode head) {
         if(head == null) return false;
         
         ListNode slow = head, fast = head.next;
-        while(slow != null && fast != null){
-            if(slow.val == fast.val) return true;
-            if(slow == null || fast == null || fast.next == null) return false;
+        while (slow != null && fast != null) {
+            if (slow.val == fast.val) return true;
+            if (slow.next == null || fast.next == null || fast.next.next == null) return false;
+            
             slow = slow.next;
             fast = fast.next.next;
         }
+        
         return false;
     }
 }
