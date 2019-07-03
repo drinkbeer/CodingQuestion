889c3aa48360084f3c400f217d51de3d72c13e2b
Jason Chen <chenjb90@gmail.com>
2019-07-03 11:23:09 -0700

    Update LeetCode-239-Sliding-Window-Maximum.java

--- src/LeetCode-239-Sliding-Window-Maximum.java
+++ src/LeetCode-239-Sliding-Window-Maximum.java
@@ -26,12 +26,17 @@ class Solution {
     Runtime: 9 ms, faster than 82.32% of Java online submissions for Sliding Window Maximum.
     Memory Usage: 41.2 MB, less than 85.19% of Java online submissions for Sliding Window Maximum.
     O(N) time complexity.
-    
-    All these operations are to the tail:
-    dq.peek();
-    dq.poll();
+    The Window Range is [i - k + 1, i]
     
     All these operations are to the head:
+    dq.peek();
+    dq.peekFirst();
+    dq.poll();
+    dq.pollFirst();
+    
+    All these operations are to the tail:
+    dq.offer();
+    dq.add();
     dq.peekLast();
     dq.pollLast();
     */
@@ -39,20 +44,27 @@ class Solution {
         if (nums == null || nums.length == 0) return new int[0];
         int[] res = new int[nums.length - k + 1];
         
+        // Using Deque to store the index in the array
+        // using res array to store the result value
         Deque<Integer> dq = new ArrayDeque<>();
         for (int i = 0; i < nums.length; i++) {
             // the window index range is [i - k + 1, i]
-            if (!dq.isEmpty() && dq.peek() < i - k + 1) {
-                dq.poll();
+            
+            // Remove the element that is out of range k in the window
+            if (!dq.isEmpty() && dq.peekFirst() < i - k + 1) {
+                dq.pollFirst();
             }
             
+            // Remote the smaller elements than nums[i] from right to left in the window
+            // This is because if nums[x] <= nums[i], x < i, then nums[x] has no chance to become max
+            // in [i - k + 1, i], or any other subsequent window: nums[i] would always be a better candidate
             while(!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
                 dq.pollLast();
             }
             
             dq.offer(i);
             if (i - k + 1 >= 0) {
-                res[i - k + 1] = nums[dq.peek()];
+                res[i - k + 1] = nums[dq.peekFirst()];
             }
         }
         
