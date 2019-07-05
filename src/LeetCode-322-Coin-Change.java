780d5872f81f9d9162d9afaf45ca335904f72e80
Jason Chen <chenjb90@gmail.com>
2019-07-05 12:32:53 -0700

    Update LeetCode-322-Coin-Change.java

--- src/LeetCode-322-Coin-Change.java
+++ src/LeetCode-322-Coin-Change.java
@@ -51,30 +51,113 @@ class Solution {
 //         }
 //     }
     
-    // 2.DP
+    
+//     public int coinChange(int[] coins, int amount) {
+//         if (coins == null || coins.length == 0) return -1;
+        
+//         Arrays.sort(coins);
+//         int[] res = new int[1];
+//         res[0] = Integer.MAX_VALUE;
+//         DFS(coins, amount, coins.length - 1, 0, res);
+//         return res[0] == Integer.MAX_VALUE ? -1 : res[0];
+//     }
+    
+//     private void DFS(int[] coins, int amount, int start, int count, int[] res) {
+//         if (start < 0) return;
+//         if (amount % coins[start] == 0) {
+//             // means the remaining amount could all use coins[i] to fill
+//             res[0] = Math.min(res[0], count + amount / coins[start]);
+//             return;
+//         }
+        
+//         // amount / coins[start] is number of coins[start] selected. We search from most amount of coins[start] to 0
+//         // as we are search from largest coins to smallest, so we search in reverse order in order to get most larger coins to make
+//         // we get the smallest number of coins faster
+//         for (int i = amount / coins[start]; i >= 0; i--) {
+//             if (i + count > res[0]) return;
+            
+//             DFS(coins, amount - i * coins[start], start - 1, count + i, res);
+//         }
+//     }
+    
+    // DFS. recerse order (looks like there is some proble using Arrays.sort, but the approach is viable)
+//     public int coinChange(int[] coins, int amount) {
+//         if (coins == null || coins.length == 0) return -1;
+        
+//         // Arrays.sort(coins);
+//         Arrays.sort(coins, (a, b) -> (int) b - (int) a);
+//         int[] res = new int[1];
+//         res[0] = Integer.MAX_VALUE;
+//         DFS(coins, amount, 0, 0, res);
+//         return res[0] == Integer.MAX_VALUE ? -1 : res[0];
+//     }
+    
+//     private void DFS(int[] coins, int amount, int start, int count, int[] res) {
+//         if (amount % coins[start] == 0) {
+//             // means the remaining amount could all use coins[i] to fill
+//             res[0] = Math.min(res[0], count + amount / coins[start]);
+//             return;
+//         }
+        
+//         // amount / coins[start] is number of coins[start] selected. We search from most amount of coins[start] to 0
+//         // as we are search from largest coins to smallest, so we search in reverse order in order to get most larger coins to make
+//         // we get the smallest number of coins faster
+//         for (int i = amount / coins[start]; i >= 0; i--) {
+//             if (i + count > res[0]) return;
+//             if (start + 1 >= coins.length) return;
+            
+//             DFS(coins, amount - i * coins[start], start + 1, count + i, res);
+//         }
+//     }
+    
+    
+    // 2.DP (pull)
     /*
     https://www.cnblogs.com/grandyang/p/5138186.html
     https://leetcode.com/problems/coin-change/discuss/77360/C%2B%2B-O(n*amount)-time-O(amount)-space-DP-solution
+    
+    subproblem:
+    dp[i]    -   the min number of coins to construct amount i
+    
+    recurrence relation:
+    Assuming that dp [0, i - 1] has already been the most optimized number of coins to change
+    dp[i] = min(dp[i], dp[i - coins[j]]), i belongs to [1, amount], j belongs to [0, coins.length)
+    
+    example: for i amount, it could be transit to three amount: from dp[i - 1] plus 1, from dp[i - 2] plus 2, from dp[i - 5] plus 5
+    
+    init:
+    dp[0] = 0;  // amount is 0, so the number of coins is 0
+    
+    ans:
+    dp[amount]
+    
     */
 //     public int coinChange(int[] coins, int amount) {
-//         int[] state = new int[amount + 1];
-//         Arrays.fill(state, amount + 1);
-//         state[0] = 0;
+//         // subproblem:
+//         int[] dp = new int[amount + 1];
         
+//         // init
+//         Arrays.fill(dp, amount + 1);
+//         dp[0] = 0;
+        
+//         // recurrence relation
 //         for (int i = 1; i <= amount; i++) {
 //             for (int j = 0; j < coins.length; j++) {
-//                 if (coins[j] <= i) {
-//                     state[i] = Math.min(state[i], state[i - coins[j]] + 1);
+//                 if (i - coins[j] >= 0) {
+//                     dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
 //                 }
 //             }
 //         }
         
-//         return state[amount] == amount + 1 ? -1 : state[amount];
+//         // ans
+//         return dp[amount] == amount + 1 ? -1 : dp[amount];
 //     }
     
     // 3.DP
     /**
     http://zxi.mytechroad.com/blog/dynamic-programming/leetcode-322-coin-change/
+    
+    if num of coins is <<<<< amount, then it's better to loop the coin outside.
     */
     public int coinChange(int[] coins, int amount) {
         int[] state = new int[amount + 1];
