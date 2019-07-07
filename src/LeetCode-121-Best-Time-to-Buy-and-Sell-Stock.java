15ca36451a2df96a759253c0bbc5b4bd71e6a466
Jason Chen <chenjb90@gmail.com>
2019-07-06 22:50:27 -0700

    Update LeetCode-121-Best-Time-to-Buy-and-Sell-Stock.java

--- src/LeetCode-121-Best-Time-to-Buy-and-Sell-Stock.java
+++ src/LeetCode-121-Best-Time-to-Buy-and-Sell-Stock.java
@@ -7,35 +7,94 @@ ProgramCreek:  http://www.programcreek.com/2014/02/leetcode-best-time-to-buy-and
 Analysis:  
 DP
 */
-public class Solution {
+class Solution {
+
+    // 1. Two Way Scan
+    /*
+    first array: left min view
+    second array: right max view
     
-    // 1. DP
+    Time O(N)
+    Space O(N)
+    */
 //     public int maxProfit(int[] prices) {
 //         if (prices == null || prices.length == 0) return 0;
         
-//         int[] state = new int[prices.length];
-//         int lowest = prices[0];
+//         int n = prices.length;
         
-//         for (int i = 1; i < prices.length; i++) {
-//             lowest = Math.min(lowest, prices[i]);
-//             state[i] = Math.max(state[i - 1], prices[i] - lowest);
+//         int[] left = new int[n];
+//         int leftMin = Integer.MAX_VALUE;
+//         for (int i = 0; i < n; i++) {
+//             leftMin = Math.min(leftMin, prices[i]);
+//             left[i] = leftMin;
 //         }
         
-//         return state[prices.length - 1];
+//         int[] right = new int[n];
+//         int rightMax = Integer.MIN_VALUE;
+//         for (int i = n - 1; i >= 0; i--) {
+//             rightMax = Math.max(rightMax, prices[i]);
+//             right[i] = rightMax;
+//         }
+        
+//         int max = Integer.MIN_VALUE;
+//         for (int i = 0; i < n; i++) {
+//             if (left[i] < right[i]) {
+//                 max = Math.max(max, right[i] - left[i]);
+//             }
+//         }
+        
+//         return max == Integer.MIN_VALUE ? 0 : max;
 //     }
     
-    // 2.Greedy
+    // 2. DP (Pull)
+    /*
+    subproblem:
+    dp[i]   -   the highest prices by ith day
+    
+    recurrence:
+    dp[i] = max(dp[i - 1], prices[i] - min);
+    
+    init:
+    dp[0] = 0
+    
+    ans:
+    dp[n - 1]
+    */
+//     public int maxProfit(int[] prices) {
+//         if (prices == null || prices.length == 0) return 0;
+
+//         int n = prices.length;
+
+//         // subproblem
+//         int[] dp = new int[n];
+
+//         // init
+//         dp[0] = 0;
+//         int min = prices[0];
+        
+//         // recurrence relation
+//         for (int i = 1; i < n; i++) {
+//             min = Math.min(min, prices[i]);
+//             dp[i] = Math.max(dp[i - 1], prices[i] - min);
+//         }
+        
+//         // ans
+//         return dp[n - 1];
+//     }
+    
+    // 3. Greedy
     public int maxProfit(int[] prices) {
-        if(prices == null || prices.length <= 1) return 0;
+        if (prices == null || prices.length == 0) return 0;
+
+        int n = prices.length;
+    
+        int min = prices[0], maxProfit = 0;
         
-        int maxValue = 0;
-        int min = prices[0];
-        
-        for(int i = 1; i < prices.length; i++){
+        for (int i = 1; i < n; i++) {
             min = Math.min(min, prices[i]);
-            maxValue = Math.max(maxValue, prices[i] - min);
+            maxProfit = Math.max(maxProfit, prices[i] - min);
         }
         
-        return maxValue;
+        return maxProfit;
     }
 }
