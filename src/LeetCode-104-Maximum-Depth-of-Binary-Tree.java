0397d1354d38c2c1a3a2424bb5a1f5920b866a52
Jason Chen <chenjb90@gmail.com>
2019-07-06 20:31:31 -0700

    Update LeetCode-104-Maximum-Depth-of-Binary-Tree.java

--- src/LeetCode-104-Maximum-Depth-of-Binary-Tree.java
+++ src/LeetCode-104-Maximum-Depth-of-Binary-Tree.java
@@ -15,9 +15,31 @@ ProgramCreek:
  * }
  */
 public class Solution {
+    // 1.DFS
+    // public int maxDepth(TreeNode root) {
+    //     if(root == null) return 0;
+        
+    //     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    // }
+    
+    // 2.BFS (level order traversal - Queue)
     public int maxDepth(TreeNode root) {
         if(root == null) return 0;
         
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int depth = 0;
+        while(!queue.isEmpty()){
+            depth++;
+            int size = queue.size();
+            
+            for(int i = 0; i < size; i++){
+                TreeNode curr = queue.poll();
+                if(curr.left != null) queue.offer(curr.left);
+                if(curr.right != null) queue.offer(curr.right);
+            }
+        }
+        
+        return depth;
     }
 }
