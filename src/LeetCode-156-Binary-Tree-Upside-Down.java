a6f13540510908f411a9889054ed939621fcc54c
Jason Chen <chenjb90@gmail.com>
2019-07-07 18:14:18 -0700

    Update LeetCode-156-Binary-Tree-Upside-Down.java

--- src/LeetCode-156-Binary-Tree-Upside-Down.java
+++ src/LeetCode-156-Binary-Tree-Upside-Down.java
@@ -8,38 +8,45 @@
  * }
  */
 class Solution {
-    // 1.DFS
-//     public TreeNode upsideDownBinaryTree(TreeNode root) {
-//         if(root == null || root.left == null) {
-//             return root;
-//         }
 
+    // 1. Recursive
+//     public TreeNode upsideDownBinaryTree(TreeNode root) {
+//         if (root == null || root.left == null) return root;
+        
 //         TreeNode newRoot = upsideDownBinaryTree(root.left);
-//         root.left.left = root.right;   // node 2 left children
-//         root.left.right = root;         // node 2 right children
+//         root.left.left = root.right;
+//         root.left.right = root;
 //         root.left = null;
 //         root.right = null;
+        
 //         return newRoot;
 //     }
     
-    // 2.BFS
+    // 2.Iterative
+    /*
+    The idea is the move the tree down to its left child.
+    Record prev node, and prev node's right child.
+    
+    curr.left = prev node's right child
+    curr.right = prev
+    */
     public TreeNode upsideDownBinaryTree(TreeNode root) {
-        TreeNode curr = root;
-        TreeNode next = null;
-        TreeNode temp = null;
-        TreeNode prev = null;
-
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root, prev = null, prevRight = null;
+        
         while(curr != null) {
-            next = curr.left;
-
-            // swapping nodes now, need temp to keep the previous right child
-            curr.left = temp;
-            temp = curr.right;
+            TreeNode nextLeft = curr.left;
+            
+            // swapping nodes now
+            curr.left = prevRight;
+            prevRight = curr.right;
             curr.right = prev;
-
+            
+            // move to next left node
             prev = curr;
-            curr = next;
+            curr = nextLeft;
         }
+        
         return prev;
-    }  
+    }
 }
