72a16212c34daee2ab07aef15fc050c134da224b
Jason Chen <chenjb90@gmail.com>
2019-07-08 20:01:01 -0700

    Update LeetCode-256-Paint-House.java

--- src/LeetCode-256-Paint-House.java
+++ src/LeetCode-256-Paint-House.java
@@ -2,40 +2,47 @@
 
 
 */
-public class Solution {
-    public int minCost(int[][] costs) {
-        if(costs == null || costs.length == 0) return 0;
+class Solution {
+//     public int minCost(int[][] costs) {
+//         if (costs == null || costs.length == 0) return 0;
         
-        int[][] state = new int[costs.length][3];
-        // init state
-        state[0][0] = costs[0][0];
-        state[0][1] = costs[0][1];
-        state[0][2] = costs[0][2];
+//         int n = costs.length;
+//         int[][] dp = new int[n][3];
+//         dp[0][0] = costs[0][0];
+//         dp[0][1] = costs[0][1];
+//         dp[0][2] = costs[0][2];
         
-        for(int i = 1; i < costs.length; i++){
-            state[i][0] = Math.min(state[i - 1][1], state[i - 1][2]) + costs[i][0];
-            state[i][1] = Math.min(state[i - 1][0], state[i - 1][2]) + costs[i][1];
-            state[i][2] = Math.min(state[i - 1][0], state[i - 1][1]) + costs[i][2];
-        }
+//         for (int i = 1; i < n; i++) {
+//             dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
+//             dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
+//             dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
+//         }
         
-        int result = Math.min(state[costs.length - 1][0], state[costs.length - 1][1]);
-        return Math.min(result, state[costs.length - 1][2]);
-    }
+//         return Math.min(dp[n - 1][0], Math.min(dp[n - 1][1], dp[n - 1][2]));
+        
+//     }
+    
     
     public int minCost(int[][] costs) {
         if (costs == null || costs.length == 0) return 0;
         
         int n = costs.length;
-        int[][] state = new int[n][3];
-        for (int i = 0; i < 3; i++) state[0][i] = costs[0][i];
+        int c0 = costs[0][0];
+        int c1 = costs[0][1];
+        int c2 = costs[0][2];
         
         for (int i = 1; i < n; i++) {
-            state[i][0] = Math.min(state[i - 1][1], state[i - 1][2]) + costs[i][0];
-            state[i][1] = Math.min(state[i - 1][0], state[i - 1][2]) + costs[i][1];
-            state[i][2] = Math.min(state[i - 1][0], state[i - 1][1]) + costs[i][2];
+            int t0 = Math.min(c1, c2) + costs[i][0];
+            int t1 = Math.min(c0, c2) + costs[i][1];
+            int t2 = Math.min(c0, c1) + costs[i][2];
+            
+            c0 = t0;
+            c1 = t1;
+            c2 = t2;
         }
-        System.out.println(Arrays.deepToString(state));
         
-        return Math.min(state[n - 1][0], Math.min(state[n - 1][1], state[n - 1][2]));
+        return Math.min(c0, Math.min(c1, c2));
+        
     }
+    
 }
