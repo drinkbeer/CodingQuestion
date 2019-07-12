e9b75e1edd4c77466b0efadd4bab73e725e308cb
Jason Chen <chenjb90@gmail.com>
2019-07-11 17:40:55 -0700

    Update LeetCode-367-Valid-Perfect-Square.java

--- src/LeetCode-367-Valid-Perfect-Square.java
+++ src/LeetCode-367-Valid-Perfect-Square.java
@@ -12,4 +12,21 @@ class Solution {
         
         return false;
     }
+    
+    public boolean isPerfectSquare(int num) {
+        if (num == 1 || num == 2 || num == 4) return true;
+        if (num == 3) return false;
+        int lo = 1, hi = num / 2;
+        
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (mid < num / mid) lo = mid + 1;
+            else if (mid > num / mid) hi = mid - 1;
+            else return num % mid == 0;
+        }
+        
+        return false;
+    }
+    
 }
