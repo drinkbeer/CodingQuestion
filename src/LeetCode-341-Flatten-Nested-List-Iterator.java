2eb54ab95bcab8859cb2d4b7c5751e388a6915d9
Jason Chen <chenjb90@gmail.com>
2019-07-13 10:46:06 -0700

    Update LeetCode-341-Flatten-Nested-List-Iterator.java

--- src/LeetCode-341-Flatten-Nested-List-Iterator.java
+++ src/LeetCode-341-Flatten-Nested-List-Iterator.java
@@ -1,3 +1,7 @@
+/*
+Time: O(N)
+Space: O(N)
+*/
 /**
  * // This is the interface that allows for creating nested lists.
  * // You should not implement it, or speculate about its implementation
@@ -15,35 +19,70 @@
  *     public List<NestedInteger> getList();
  * }
  */
+// public class NestedIterator implements Iterator<Integer> {
+
+//     Queue<Integer> queue;
+//     public NestedIterator(List<NestedInteger> nestedList) {
+//         queue = new LinkedList<>();
+//         recursive(nestedList, queue);
+//     }
+    
+//     private void recursive(List<NestedInteger> list, Queue<Integer> queue) {
+//         for (NestedInteger ni : list) {
+//             if (ni.isInteger()) {
+//                 queue.offer(ni.getInteger());
+//             } else {
+//                 recursive(ni.getList(), queue);
+//             }
+//         }
+//     }
+
+//     @Override
+//     public Integer next() {
+//         if (!queue.isEmpty()) {
+//             return queue.poll();
+//         }
+//         return -1;
+//     }
+
+//     @Override
+//     public boolean hasNext() {
+//         return !queue.isEmpty();
+//     }
+// }
+
+// 2. Lazy Load
+/*
+There are 1000000000 Integers in nestedList and the user only calls next() once. -> You have to put 1000000000 Integers into stack, but the user only takes the 1st one. Instead, we should push to stack "lazy".
+
+*/
 public class NestedIterator implements Iterator<Integer> {
 
-    Queue<Integer> queue;
+    Stack<NestedInteger> stack;
     public NestedIterator(List<NestedInteger> nestedList) {
-        queue = new LinkedList<>();
-        recursive(nestedList, queue);
-    }
-    
-    private void recursive(List<NestedInteger> list, Queue<Integer> queue) {
-        for (NestedInteger ni : list) {
-            if (ni.isInteger()) {
-                queue.offer(ni.getInteger());
-            } else {
-                recursive(ni.getList(), queue);
-            }
-        }
+        stack = new Stack<>();
+        recursive(nestedList, stack);
     }
 
     @Override
     public Integer next() {
-        if (!queue.isEmpty()) {
-            return queue.poll();
-        }
-        return -1;
+        if (!hasNext()) throw new java.util.NoSuchElementException();
+        return stack.pop().getInteger();
     }
 
     @Override
     public boolean hasNext() {
-        return !queue.isEmpty();
+        while (!stack.isEmpty() && !stack.peek().isInteger()) {
+            recursive(stack.pop().getList(), stack);
+        }
+        
+        return !stack.isEmpty();
+    }
+    
+    private void recursive(List<NestedInteger> list, Stack<NestedInteger> stack) {
+        for (int i = list.size() - 1; i >= 0; i--) {
+            stack.push(list.get(i));
+        }
     }
 }
 
