effc374de200055dde67a4e507b712ed165eda03
Jason Chen <chenjb90@gmail.com>
2019-07-31 10:19:26 -0700

    Update LeetCode-18-4Sum.java

--- src/LeetCode-18-4Sum.java
+++ src/LeetCode-18-4Sum.java
@@ -4,7 +4,7 @@ LintCode: http://www.lintcode.com/problem/4sum/
 JiuZhang: http://www.jiuzhang.com/solutions/4sum/
 ProgramCreek: http://www.programcreek.com/2013/02/leetcode-4sum-java/
 
-Analysis: (Two Pointer solution. The same solution with 3Sum)
+Analysis: (Two Pointers solution. The same solution with 3Sum)
 1.Sort array
 2.P1 scan from 0 to Len-3
 2.P2 scan from p1+1 to Len-2
@@ -14,54 +14,96 @@ Analysis: (Two Pointer solution. The same solution with 3Sum)
 这道题还有另一个方法，就是先可以用一个hash表记录数组当中任意两个数的和sum = a[x]+a[y]，并且记录是哪两个数的和, 这样用o(n^2)的时间复杂度就可以记录任意两个数的和，然后在用两个指针i,j(i<j)遍历数组, 然后在hash表中寻找v-a[i]-a[j]是否存在，并且保证a[i],a[j],a[x],a[y] 不能取相同的数，然后通过i,j两层for循环就可以找到4sum
 */
 public class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        if(nums == null && nums.length < 4) return result;
+    // 1.Two Pointers, just like 3Sum
+//     public List<List<Integer>> fourSum(int[] nums, int target) {
+//         List<List<Integer>> result = new ArrayList<List<Integer>>();
+//         if(nums == null && nums.length < 4) return result;
         
-        java.util.Arrays.sort(nums);
+//         java.util.Arrays.sort(nums);
         
-        for(int i = 0; i < nums.length - 3; i++){
-            if(i != 0 && nums[i - 1] == nums[i]) continue;       //skip the duplicates
+//         for(int i = 0; i < nums.length - 3; i++){
+//             if(i != 0 && nums[i - 1] == nums[i]) continue;       //skip the duplicates
             
-            for(int j = i + 1; j < nums.length - 2; j++){
-                if(j != i + 1 && nums[j - 1] == nums[j]) continue;       //skip the duplicates
+//             for(int j = i + 1; j < nums.length - 2; j++){
+//                 if(j != i + 1 && nums[j - 1] == nums[j]) continue;       //skip the duplicates
                 
-                int lo = j + 1;
-                int hi = nums.length - 1;
+//                 int lo = j + 1;
+//                 int hi = nums.length - 1;
                 
-                while(lo < hi){
-                    int sum = nums[i] + nums[j] + nums[lo] + nums[hi];
-                    if(sum < target){
-                        lo++;
-                    }else if(sum > target){
-                        hi--;
-                    }else{
-                        List<Integer> temp = new ArrayList<Integer>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[lo]);
-                        temp.add(nums[hi]);
-                        result.add(temp);
+//                 while(lo < hi){
+//                     int sum = nums[i] + nums[j] + nums[lo] + nums[hi];
+//                     if(sum == target){
+//                         List<Integer> temp = new ArrayList<Integer>();
+//                         temp.add(nums[i]);
+//                         temp.add(nums[j]);
+//                         temp.add(nums[lo]);
+//                         temp.add(nums[hi]);
+//                         result.add(temp);
                         
-                        lo++;
-                        hi--;
+//                         lo++;
+//                         hi--;
                         
-                        //skip duplicates
-                        while(lo < hi && nums[lo - 1] == nums[lo]){
-                            lo++;
-                        }
+//                         //skip duplicates
+//                         while(lo < hi && nums[lo - 1] == nums[lo]) lo++;
+//                         while(lo < hi && nums[hi] == nums[hi + 1]) hi--;
                         
-                        while(lo < hi && nums[hi] == nums[hi + 1]){
-                            hi--;
-                        }
+//                     }else if(sum < target){
+//                         lo++;
+//                     }else{
+//                         hi--;
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return result;
+//     }
+    
+    // 2.HashTable (How to record the two numbers in HashTable? If key is sum, what can we do if key duplicates, 1+5==2+4)
+    // public List<List<Integer>> fourSum(int[] nums, int target) {
+        
+        
+    // }
+    
+    
+        /*
+    [1,0,-1,0,-2,2] -   [-2,-1,0,0,1,2]
+    0
+    */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        if (nums == null || nums.length <= 3) return new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length - 3; i++) {
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                
+                int t = target - nums[i] - nums[j];
+                int l = j + 1, r = nums.length - 1;
+                
+                while (l < r) {
+                    if (t == nums[l] + nums[r]) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
                         
+                        l++;
+                        r--;
+                        
+                        while (l < r && nums[l - 1] == nums[l]) l++;
+                        while (l < r && nums[r] == nums[r + 1]) r--;
+                    } else if (t > nums[l] + nums[r]) {
+                        l++;
+                    } else {
+                        r--;
                     }
-                    
                 }
                 
+                while(j < nums.length - 2 && nums[j] == nums[j + 1]) j++;
             }
+            while(i < nums.length - 3 && nums[i] == nums[i + 1]) i++;
         }
         
-        return result;
+        return res;
     }
+    
 }
