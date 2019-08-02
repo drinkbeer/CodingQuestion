9b48e41c439e9ba413030aebf6475ef0be5e4bbf
Jason Chen <chenjb90@gmail.com>
2019-08-02 16:02:51 -0700

    Update LeetCode-252-Meeting-Rooms.java

--- src/LeetCode-252-Meeting-Rooms.java
+++ src/LeetCode-252-Meeting-Rooms.java
@@ -18,22 +18,36 @@ Analysis:
  * }
  */
 public class Solution {
-    public boolean canAttendMeetings(Interval[] intervals) {
-        if(intervals == null || intervals.length <= 1) return true;
+//     public boolean canAttendMeetings(Interval[] intervals) {
+//         if(intervals == null || intervals.length <= 1) return true;
         
-        java.util.Arrays.sort(intervals, new IntervalComparator());
-        for(int i = 1; i < intervals.length; i++){
-            Interval prev = intervals[i - 1];
-            Interval curr = intervals[i];
-            if(prev.end > curr.start) return false;
+//         java.util.Arrays.sort(intervals, new IntervalComparator());
+//         for(int i = 1; i < intervals.length; i++){
+//             Interval prev = intervals[i - 1];
+//             Interval curr = intervals[i];
+//             if(prev.end > curr.start) return false;
+//         }
+        
+//         return true;
+//     }
+    
+//     private class IntervalComparator implements Comparator<Interval>{
+//         public int compare(Interval p, Interval q){
+//             return p.start - q.start;
+//         }
+//     }
+    
+    
+    public boolean canAttendMeetings(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) return true;
+        Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        
+        int e = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (e > intervals[i][0]) return false;
+            e = intervals[i][1];
         }
         
         return true;
-    }
-    
-    private class IntervalComparator implements Comparator<Interval>{
-        public int compare(Interval p, Interval q){
-            return p.start - q.start;
-        }
     }
 }
