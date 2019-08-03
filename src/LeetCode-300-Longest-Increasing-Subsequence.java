a23f9c969afaf0f787f52c6d86e2b34690cc7eeb
Jason Chen <chenjb90@gmail.com>
2019-08-02 17:57:33 -0700

    Update LeetCode-300-Longest-Increasing-Subsequence.java

--- src/LeetCode-300-Longest-Increasing-Subsequence.java
+++ src/LeetCode-300-Longest-Increasing-Subsequence.java
@@ -1,11 +1,3 @@
-/*
-http://www.jiuzhang.com/solutions/longest-increasing-subsequence/
-
-1.DP
-
-2.Binary Search
-
-*/
 class Solution {
     // Wrong Answer
     /*
@@ -35,7 +27,7 @@ class Solution {
 //         return max == Integer.MIN_VALUE ? 0 : max;
 //     }
     
-    // 1. DP
+    // 1. DP (pull)
     /*
     subproblem:
     dp[i]   -   the max Len til ith number in nums array
@@ -45,6 +37,8 @@ class Solution {
     
     init:
     dp[0] = 1   only one element, so it must be increasing
+    
+    Time O(N^2)
     */
 //     public int lengthOfLIS(int[] nums) {
 //         if (nums == null || nums.length == 0) return 0;
@@ -61,8 +55,8 @@ class Solution {
 //         // recurrence relation:
 //         for (int i = 1; i < n; i++) {
 //             for (int j = i - 1; j >= 0; j--) {
-//                 if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) {
-//                     dp[i] = dp[j] + 1;
+//                 if (nums[j] < nums[i]) {
+//                     dp[i] = Math.max(dp[i], dp[j] + 1);
 //                 }
 //             }
 //             max = Math.max(max, dp[i]);
@@ -82,6 +76,48 @@ class Solution {
 
 
     https://segmentfault.com/a/1190000003819886 (讲解比较清晰，但是在Binary Search的时候，应该找的是 first number >= target而不是> target)
+    Time O(NlogN)
+    
+    Example: 
+    [1,3,5,2,8,4,6], 当到6时，我们一共可以有四种
+        (1)不同长度
+        (2)且保证该升序序列在同长度升序序列中末尾最小
+    的升序序列:
+    
+    1
+    1,2
+    1,3,4
+    1,3,5,6
+    
+    这些序列都是未来有可能成为最长序列的候选人。这样，每来一个新的数，我们便按照以下规则更新这些序列:
+    
+    (1) 如果nums[i]比所有序列的末尾都大，或等于最大末尾，说明有一个新的不同长度序列产生，我们把最长的序列复制一个，并加上这个nums[i]。
+    (2) 如果nums[i]比所有序列的末尾都小，说明长度为1的序列可以更新了，更新为这个更小的末尾。
+    (3) 如果在中间，则更新那个末尾数字刚刚大于等于自己的那个序列，说明那个长度的序列可以更新了。
+    
+    比如这时，如果再来一个9，那就是第三种情况，更新序列为
+
+    1
+    1,2
+    1,3,4
+    1,3,5,6
+    1,3,5,6,9
+
+    如果再来一个3，那就是第二种情况，更新序列为
+    1
+    1,2
+    1,3,3
+    1,3,5,6
+    
+    如果再来一个0，那就是第一种情况，更新序列为
+    0
+    1,2
+    1,3,3
+    1,3,5,6
+
+
+    前两种都很好处理，O(1)就能解决，主要是第三种情况，实际上我们观察直到6之前这四个不同长度的升序序列，他们末尾是递增的，所以可以用二分搜索来找到适合的更新位置。
+    
     
     */
     public int lengthOfLIS(int[] nums) {
