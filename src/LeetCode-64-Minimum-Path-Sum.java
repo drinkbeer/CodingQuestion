f4fd5c1510650a31cba05f28941647aafb5a019b
Jason Chen <chenjb90@gmail.com>
2021-01-31 18:44:42 -0500

    Update LeetCode-64-Minimum-Path-Sum.java

--- src/LeetCode-64-Minimum-Path-Sum.java
+++ src/LeetCode-64-Minimum-Path-Sum.java
@@ -9,30 +9,26 @@ DP
 */
 
 public class Solution {
-    // DP
-    public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+    // Recursive (will TLE)
+//     public int minPathSum(int[][] grid) {
+//         if (grid == null || grid.length == 0) return 0;
         
-        int[][] paths = new int[m][n];
-        paths[0][0] = grid[0][0];
-        for(int i = 1; i < m; i++){
-            paths[i][0] = paths[i - 1][0] + grid[i][0];
-        }
-        for(int j = 1; j < n; j++){
-            paths[0][j] = paths[0][j - 1] + grid[0][j];
-        }
-        
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                paths[i][j] = Math.min(paths[i - 1][j] + grid[i][j], paths[i][j - 1] + grid[i][j]);
-            }
-        }
-        
-        return paths[m - 1][n - 1];
-    }
+//         return helper(grid, grid.length - 1, grid[0].length - 1);
+//     }
     
-        // 1. DP, Time O(M*N), Space O(M*N)
+//     private int helper(int[][] grid, int x, int y) {
+//         if (x == 0 && y == 0) return grid[0][0];
+        
+//         if (x == 0) {
+//             return grid[0][y] + helper(grid, 0, y - 1);
+//         } else if (y == 0) {
+//             return grid[x][0] + helper(grid, x - 1, 0);
+//         } else {
+//             return grid[x][y] + Math.min(helper(grid, x, y - 1), helper(grid, x - 1, y));
+//         }
+//     }
+    
+    // 1. DP, Time O(M*N), Space O(M*N)
 //     public int minPathSum(int[][] grid) {
 //         if (grid == null) return 0;
 //         int n = grid.length, m = grid[0].length;
