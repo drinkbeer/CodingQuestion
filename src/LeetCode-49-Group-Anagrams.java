437717dbc2a7b9d62d62ffc81fbea04056e610b0
Jason Chen <chenjb90@gmail.com>
2021-03-14 14:25:40 -0400

    Update LeetCode-49-Group-Anagrams.java

--- src/LeetCode-49-Group-Anagrams.java
+++ src/LeetCode-49-Group-Anagrams.java
@@ -9,28 +9,25 @@ Using HashMap as map.
 */
 public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if(strs == null || strs.length == 0) return null;
+        List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
         
-        List<List<String>> result = new ArrayList<List<String>>();
-        HashMap<String, List<String>> map = new HashMap<String, List<String>>();
-        java.util.Arrays.sort(strs);        // to ensure result is sorted
-        
-        for(String str : strs){
-            char[] chars = str.toCharArray();
-            java.util.Arrays.sort(chars);
-            String s = new String(chars);
-            if(map.containsKey(s)){
-                map.get(s).add(str);
-            }else{
-                List<String> l = new ArrayList<String>();
-                l.add(str);
-                map.put(s, l);
-            }
+        for (String str : strs) {
+            String formatted = formatStr(str);
+            map.putIfAbsent(formatted, new ArrayList<>());
+            map.get(formatted).add(str);
         }
         
-        for(List<String> l : map.values()){
+        for(List<String> l : map.values()) {
             result.add(l);
         }
+        
         return result;
+    }
+    
+    private String formatStr(String str) {
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
 }
