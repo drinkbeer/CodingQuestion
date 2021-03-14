871b9950ec8554620157f98a3558e845e0a3b244
Jason Chen <chenjb90@gmail.com>
2021-03-14 15:59:42 -0400

    Update LeetCode-93-Restore-IP-Addresses.java

--- src/LeetCode-93-Restore-IP-Addresses.java
+++ src/LeetCode-93-Restore-IP-Addresses.java
@@ -43,21 +43,20 @@ class Solution {
 
     */
     public List<String> restoreIpAddresses(String s) {
-        List<String> result = new ArrayList<String>();
-        if(s == null || s.length() < 4 || s.length() > 12) return result;
+        List<String> result = new ArrayList<>();
+        if (s == null || s.length() < 4 || s.length() > 12) return result;
         
-        List<String> list = new ArrayList<>();
-        DFS(s, 0, result, list);
+        DFS(s, 0, result, new ArrayList<>());
         return result;
     }
     
-    private void DFS(String s, int start, List<String> result, List<String> list){
+    private void DFS(String s, int start, List<String> result, List<String> list) {
         // end condition
-        if(list.size() == 4){
-            if(start != s.length()) return;
+        if (list.size() == 4) {
+            if (start != s.length()) return;
             
-            StringBuffer sb = new StringBuffer();
-            for(String str : list){
+            StringBuilder sb = new StringBuilder();
+            for (String str : list) {
                 sb.append(str).append(".");
             }
             sb.deleteCharAt(sb.length() - 1);
@@ -65,9 +64,10 @@ class Solution {
             return;
         }
         
-        for(int i = start; i < s.length() && i <= start + 3; i++){
+        // substring [start, i], i is inclusive
+        for (int i = start; i < s.length() && i <= start + 2; i++) {
             String tmp = s.substring(start, i + 1);
-            if(isValid(tmp)){
+            if (isValid(tmp)) {
                 list.add(tmp);
                 DFS(s, i + 1, result, list);
                 list.remove(list.size() - 1);
@@ -75,8 +75,8 @@ class Solution {
         }
     }
     
-    private boolean isValid(String s){
-        if(s.charAt(0) == '0') return s.equals("0");    // to eliminate cases like "00", "10"
+    private boolean isValid(String s) {
+        if (s.charAt(0) == '0') return s.equals("0");       // to eliminate cases like "00", "01"
         int digit = Integer.valueOf(s);
         return digit >= 0 && digit <= 255;
     }
