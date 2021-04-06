4b00e2adbbf7334d5f2d0bd9eb4b5e3661ea2956
Jason Chen <chenjb90@gmail.com>
2021-04-06 13:40:38 -0400

    Update LeetCode-92-Reverse-Linked-List-II.java

--- src/LeetCode-92-Reverse-Linked-List-II.java
+++ src/LeetCode-92-Reverse-Linked-List-II.java
@@ -14,39 +14,83 @@ Reverse nodes [m, n]
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-public class Solution {
-    public ListNode reverseBetween(ListNode head, int m, int n) {
-        if(head == null || m >= n) return head;
+class Solution {
+    
+    // 1.
+//     public ListNode reverseBetween(ListNode head, int m, int n) {
+//         if(head == null || m >= n) return head;
         
-        ListNode dummy = new ListNode(0);
+//         ListNode dummy = new ListNode(0);
+//         dummy.next = head;
+//         ListNode prev1 = dummy, curr1 = head, prev2 = dummy, curr2 = head;
+//         int count = 1;
+        
+//         while(count <= n){
+//             ListNode next = curr2.next;
+            
+//             if(count == m){
+//                 prev1 = prev2;
+//                 curr1 = curr2;
+//             }
+            
+//             if(count >= m){
+//                 curr2.next = prev2;
+//             }
+            
+//             prev2 = curr2;
+//             curr2 = next;
+            
+//             count++;
+//         }
+        
+//         prev1.next = prev2;
+//         curr1.next = curr2;
+        
+//         return dummy.next;
+//     }    
+    
+    // 2.
+    /*
+    Similar to this one: https://leetcode.com/problems/reverse-nodes-in-k-group/
+    */    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || head.next == null) return head;
+        
+        ListNode dummy = new ListNode(-1);
         dummy.next = head;
-        ListNode prev1 = dummy, curr1 = head, prev2 = dummy, curr2 = head;
-        int count = 1;
+        ListNode prev = dummy;
         
-        while(count <= n){
-            ListNode next = curr2.next;
-            
-            if(count == m){
-                prev1 = prev2;
-                curr1 = curr2;
-            }
-            
-            if(count >= m){
-                curr2.next = prev2;
-            }
-            
-            prev2 = curr2;
-            curr2 = next;
-            
-            count++;
+        for (int i = 1; i <= left - 1; i++) {
+            prev = prev.next;
         }
         
-        prev1.next = prev2;
-        curr1.next = curr2;
+        ListNode next = dummy;
+        for (int i = 1; i <= right + 1; i++) {
+            next = next.next;
+        }
+        
+        reverse(prev, next);
         
         return dummy.next;
+    }
+    
+    // prev -> last -> curr -> ...... -> kthNode -> next
+    private ListNode reverse(ListNode prev, ListNode next) {
+        ListNode last = prev.next;
+        ListNode curr = prev.next.next;
+        
+        while (curr != next) {
+            last.next = curr.next;
+            curr.next = prev.next;
+            prev.next = curr;
+            
+            curr = last.next;
+        }
+        
+        return last;
     }
 }
