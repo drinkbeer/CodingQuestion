795de518b8e846e12b6874bb4b947670831f101e
Jason Chen <jason.chen@shopify.com>
2021-04-24 18:40:59 -0400

    Update Inorder Traversal

--- src/LeetCode-94-Binary-Tree-Inorder-Traversal.java
+++ src/LeetCode-94-Binary-Tree-Inorder-Traversal.java
@@ -33,8 +33,20 @@ public class Solution {
     //     result.add(root.val);
     //     if(root.right != null) inorderTraversal(root.right, result);
     // }
+
+    // Another way to write without helper function
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> result = new ArrayList<>();
+//        if (root == null) return result;
+//
+//        if (root.left != null) result.addAll(inorderTraversal(root.left));
+//        result.add(root.val);
+//        if (root.right != null) result.addAll(inorderTraversal(root.right));
+//
+//        return result;
+//    }
     
-    // 2.BFS
+    // 2.DFS with stack
     // public List<Integer> inorderTraversal(TreeNode root) {
     //     List<Integer> result = new ArrayList<Integer>();
     //     if(root == null) return result;
@@ -57,64 +69,97 @@ public class Solution {
     //     return result;
     // }
     
-    // 3.BFS(easier to understand than 2.)
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<Integer>();
-        if(root == null)return result;
-        
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        TreeNode curr = root;       //define a pointer to track nodes
-        
-        while(curr != null || !stack.isEmpty()){
-            // if curr node is not null, push to stack, and go down the tree to left
-            if(curr != null){
-                stack.push(curr);
-                curr = curr.left;
-            }else{
-                // if no left node, pop last left node, process it
-                // then let curr point to right node
-                curr = stack.pop();
-                result.add(curr.val);
-                curr = curr.right;
-            }
-        }
-        
-        return result;
-    }
+    // 3.DFS with Stack(easier to understand than 2.)
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> result = new ArrayList<Integer>();
+//        if(root == null)return result;
+//
+//        Stack<TreeNode> stack = new Stack<TreeNode>();
+//        TreeNode curr = root;       //define a pointer to track nodes
+//
+//        while(curr != null || !stack.isEmpty()){
+//            // if curr node is not null, push to stack, and go down the tree to left
+//            if(curr != null){
+//                stack.push(curr);
+//                curr = curr.left;
+//            }else{
+//                // if no left node, pop last left node, process it
+//                // then let curr point to right node
+//                curr = stack.pop();
+//                result.add(curr.val);
+//                curr = curr.right;
+//            }
+//        }
+//
+//        return result;
+//    }
     
-        // BFS, trying to do it without additional pointer (This approach will add null to the stack.)
+    // DFS, trying to do it without additional pointer (This approach will add null to the stack.)
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> result = new ArrayList<>();
+//
+//        if (root == null) return result;
+//
+//        Stack<TreeNode> stack =  new Stack<>();
+//        stack.push(root);
+//
+//        while (!stack.isEmpty()) {
+//            TreeNode curr = stack.peek();
+//
+//            if (curr != null) {
+//                stack.push(curr.left);
+//                curr = curr.left;
+//            } else {
+//                // Means no left node, pop last left node, and process it
+//                // then left curr point to right node
+//                stack.pop(); // pop last null element
+//
+//                if (!stack.isEmpty()) {
+//                    curr = stack.pop();
+//                    result.add(curr.val);
+//
+//                    // After we proceed the last left element, let's move right
+//                    stack.push(curr.right);
+//                    curr = curr.right;
+//                }
+//
+//            }
+//        }
+//
+//        return result;
+//    }
+
+    // 4. Morris Inorder Traversal
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        
         if (root == null) return result;
-        
-        Stack<TreeNode> stack =  new Stack<>();
-        stack.push(root);
-        
-        while (!stack.isEmpty()) {
-            TreeNode curr = stack.peek();
-            
-            if (curr != null) {
-                stack.push(curr.left);
-                curr = curr.left;
-            } else {
-                // Means no left node, pop last left node, and process it
-                // then left curr point to right node
-                stack.pop(); // pop last null element
-                
-                if (!stack.isEmpty()) {
-                    curr = stack.pop();
-                    result.add(curr.val);
 
-                    // After we proceed the last left element, let's move right
-                    stack.push(curr.right);
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left == null) {
+                result.add(curr.val);
+                curr = curr.right;
+            } else {
+                // find the predecessor of curr node
+                TreeNode pre = curr.left;
+                while (pre.right != null && pre.right != curr) {
+                    pre = pre.right;
+                }
+
+                if (pre.right == null) {
+                    pre.right = curr;
+                    curr = curr.left;
+                } else {
+                    // finished traverse all the left tree, so process root now
+                    // reset the predecessor's right to null
+                    // as we finished traverse all left tree, and root, we should move to right tree
+                    result.add(curr.val);
+                    pre.right = null;
                     curr = curr.right;
                 }
-                
             }
         }
-        
+
         return result;
     }
-    
 }
