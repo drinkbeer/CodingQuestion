7d6b7f3d52d360d0104bd6ad617b45b91ead21cd
Jason Chen <chenjb90@gmail.com>
2021-05-01 18:08:43 -0400

    Create LeetCode-99-Recover-Binary-Search-Tree.java

---
+++ src/LeetCode-99-Recover-Binary-Search-Tree.java
@@ -0,0 +1,106 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    /*
+    Space: O(N)
+    Time: O(N)
+    */
+//     public void recoverTree(TreeNode root) {
+//         if (root == null) return;
+        
+//         TreeNode curr = root;
+//         TreeNode firstElement = null, secondElement = null, prev = null;
+//         Stack<TreeNode> stack = new Stack<>();
+//         while (curr != null || !stack.isEmpty()) {
+//             if (curr != null) {
+//                 stack.push(curr);
+//                 curr = curr.left;
+//             } else {
+//                 curr = stack.pop();
+                
+//                 if (prev != null && firstElement == null && prev.val > curr.val) {
+//                     firstElement = prev;
+//                 }
+                
+//                 if (prev != null && firstElement != null && prev.val > curr.val) {
+//                     secondElement = curr;
+//                 }
+                
+//                 prev = curr;
+//                 curr = curr.right;
+//             }
+//         }
+        
+//         // swap the value of first and second element
+//         int temp = firstElement.val;
+//         firstElement.val = secondElement.val;
+//         secondElement.val = temp;
+//     }
+    
+    /*
+    Morris Traversal
+    
+    Space: O(1)
+    Time: O(N)
+    */
+    public void recoverTree(TreeNode root) {
+        if (root == null) return;
+        
+        TreeNode curr = root;
+        TreeNode prev = null, firstElement = null, secondElement = null;
+        while (curr != null) {
+            if (curr.left == null) {
+                if (prev != null && firstElement == null && prev.val > curr.val) {
+                    firstElement = prev;
+                }
+                if (prev != null && firstElement != null && prev.val > curr.val) {
+                    secondElement = curr;
+                }
+                prev = curr;
+                curr = curr.right;
+            } else {
+                TreeNode predecessor = curr.left;
+                while (predecessor.right != null && predecessor.right != curr) {
+                    predecessor = predecessor.right;
+                }
+                
+                if (predecessor.right == null) {
+                    predecessor.right = curr;
+                    curr = curr.left;
+                } else {
+                    // process curr node
+                    if (prev != null && firstElement == null && prev.val > curr.val) {
+                        firstElement = prev;
+                    }
+                    if (prev != null && firstElement != null && prev.val > curr.val) {
+                        secondElement = curr;
+                    }
+                    predecessor.right = null;
+                    prev = curr;
+                    curr = curr.right;
+                }
+            }
+        }
+        
+        // swap the value of first and second element
+        int temp = firstElement.val;
+        firstElement.val = secondElement.val;
+        secondElement.val = temp;
+    }
+    
+    
+}
