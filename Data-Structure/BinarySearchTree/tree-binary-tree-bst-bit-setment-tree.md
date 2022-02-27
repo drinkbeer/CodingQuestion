54dfe22988d0fcef4f67e2c4c8979158e19baf9e
Jianbin Chen <jason.chen@shopify.com>
2022-02-27 00:33:19 -0800

    Update problem

--- Data-Structure/BinarySearchTree/tree-binary-tree-bst-bit-setment-tree.md
+++ Data-Structure/BinarySearchTree/tree-binary-tree-bst-bit-setment-tree.md
@@ -248,8 +248,14 @@ Using Merge Sort (Divide & Conquer):
 ## Binary Index Tree (Fenwick Tree)
 https://www.youtube.com/watch?v=CWDQJGaN1gY  
 https://www.youtube.com/watch?v=WbafSgetDDk  
+https://blog.csdn.net/Yaokai_AssultMaster/article/details/79492190
 
 Binary Index Tree is proposed to solve the prefix problem. The idea is to store partial sum in each node, and get total sum by traversing the tree from leaf to root. The tree has a height of log(N).
+
+Binary Index Tree is good at:
+* update(idx, num)
+* prefixSum(idx)
+* rangeSum(from, to)
 
 Query: `O(logN)`  
 Update: `O(logN)`
--- src/LeetCode-912-Sort-an-Array.java
+++ src/LeetCode-912-Sort-an-Array.java
@@ -1,4 +1,5 @@
 class Solution {
+    // Quick Sort
     public int[] sortArray(int[] nums) {
         quickSort(nums, 0, nums.length - 1);
         return nums;
@@ -29,6 +30,38 @@ class Solution {
         nums[hi] = temp;
         
         return lo;
+    }
+
+    // Merge Sort
+    public int[] sortArray(int[] nums) {
+        int[] temp = new int[nums.length];
+        mergeSort(nums, temp, 0, nums.length - 1);
+        
+        return nums;
+    }
+    
+    private void mergeSort(int[] nums, int[] temp, int lo, int hi) {
+        if (lo >= hi) return;
+        
+        int mid = lo + (hi - lo) / 2;
+        mergeSort(nums, temp, lo, mid);
+        mergeSort(nums, temp, mid + 1, hi);
+        merge(nums, temp, lo, mid, hi);
+    }
+    
+    private void merge(int[] nums, int[] temp, int lo, int mid, int hi) {
+        for (int k = lo; k <= hi; k++) {
+            temp[k] = nums[k];
+        }
+        
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (j > hi || (i <= mid && temp[i] <= temp[j])) {
+                nums[k] = temp[i++];
+            } else {
+                nums[k] = temp[j++];
+            }
+        }
     }
     
     
