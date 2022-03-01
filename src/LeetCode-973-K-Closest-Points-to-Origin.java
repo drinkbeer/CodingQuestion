3eb745bc8ccb36d2f45dfa2e4cd53f8a9abd63ae
Jianbin Chen <jason.chen@shopify.com>
2022-02-28 20:37:05 -0800

    Update 973

--- src/LeetCode-973-K-Closest-Points-to-Origin.java
+++ src/LeetCode-973-K-Closest-Points-to-Origin.java
@@ -1,3 +1,10 @@
+/*
+Similar to
+
+215. Kth Largest Element in an Array
+https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+*/
 class Solution {
     // 1. TreeMap
     /*
@@ -56,5 +63,102 @@ class Solution {
     //     return Arrays.copyOfRange(points, 0, K);
     // }
     
-    // 4. QuickSelect
+    // 4. QuickSort -> migrate to QuickSelect
+    /*
+    Time O(NlogN)
+    Runtime: 810 ms, faster than 5.02% of Java online submissions for K Closest Points to Origin.
+    Memory Usage: 118.6 MB, less than 12.60% of Java online submissions for K Closest Points to Origin.
+    */
+//     public int[][] kClosest(int[][] points, int K) {
+//         quickSort(points, 0, points.length - 1);
+        
+//         int[][] res = new int[K][2];
+//         while (K - 1 >= 0) {
+//             res[K - 1] = points[K - 1];
+//             K--;
+//         }
+//         return res;
+//     }
+    
+//     public void quickSort(int[][] points, int lo, int hi) {
+//         if (lo >= hi) return;
+//         int partition = partition(points, lo, hi);
+//         quickSort(points, lo, partition - 1);
+//         quickSort(points, partition + 1, hi);
+//     }
+    
+//     private int partition(int[][] points, int lo, int hi) {
+//         int pivot = points[hi][0] * points[hi][0] + points[hi][1] * points[hi][1];
+//         int s = lo;
+//         for (int i = lo; i < hi; i++) {
+//             if (points[i][0] * points[i][0] + points[i][1] * points[i][1] < pivot) {
+//                 // swap i and s
+//                 int[] temp = points[s];
+//                 points[s] = points[i];
+//                 points[i] = temp;
+                
+//                 s++;
+//             }
+//         }
+        
+//         // swap hi with s
+//         int[] temp = points[s];
+//         points[s] = points[hi];
+//         points[hi] = temp;
+        
+//         return s;
+//     }
+    
+    // QuickSelect
+    /*
+    Avg Time: O(N)
+    Worst Time: O(N^2)
+    
+    Runtime: 527 ms, faster than 5.02% of Java online submissions for K Closest Points to Origin.
+    Memory Usage: 118.2 MB, less than 17.29% of Java online submissions for K Closest Points to Origin.
+    */
+    public int[][] kClosest(int[][] points, int K) {
+        quickSort(points, K, 0, points.length - 1);
+        
+        int[][] res = new int[K][2];
+        while (K - 1 >= 0) {
+            res[K - 1] = points[K - 1];
+            K--;
+        }
+        return res;
+    }
+    
+    public void quickSort(int[][] points, int K, int lo, int hi) {
+        if (lo >= hi) return;
+        int partition = partition(points, lo, hi);
+        if (partition == K) {
+            return;
+        } else if (partition < K) {
+            quickSort(points, K, partition + 1, hi);
+        } else {
+            quickSort(points, K, lo, partition - 1);
+        }
+    }
+    
+    private int partition(int[][] points, int lo, int hi) {
+        int pivot = points[hi][0] * points[hi][0] + points[hi][1] * points[hi][1];
+        int s = lo;
+        for (int i = lo; i < hi; i++) {
+            if (points[i][0] * points[i][0] + points[i][1] * points[i][1] < pivot) {
+                // swap i and s
+                int[] temp = points[s];
+                points[s] = points[i];
+                points[i] = temp;
+                
+                s++;
+            }
+        }
+        
+        // swap hi with s
+        int[] temp = points[s];
+        points[s] = points[hi];
+        points[hi] = temp;
+        
+        return s;
+    }
 }
