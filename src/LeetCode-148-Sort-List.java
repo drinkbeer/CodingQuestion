b11018df6dabef4944000fc6c13dfbe3260eae89
Jianbin Chen <jason.chen@shopify.com>
2022-03-03 19:05:13 -0800

    Update 148

--- src/LeetCode-148-Sort-List.java
+++ src/LeetCode-148-Sort-List.java
@@ -58,4 +58,52 @@ class Solution {
         
         return dummy.next;
     }
+
+
+    // Another way to split the list into two halves
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        
+        // ListNode prev = null, slow = head, fast = head;
+        // while (fast != null && fast.next != null) {
+        //     prev = slow;
+        //     slow = slow.next;
+        //     fast = fast.next.next;
+        // }
+        
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        fast = slow.next;
+        slow.next = null;
+        
+        ListNode left = sortList(head);
+        ListNode right = sortList(fast);
+        
+        return merge(left, right);
+    }
+    
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                curr.next = left;
+                left = left.next;
+            } else {
+                curr.next = right;
+                right = right.next;
+            }
+            curr = curr.next;
+        }
+        
+        if (left != null) curr.next = left;
+        if (right != null) curr.next = right;
+        
+        return dummy.next;
+    }
 }
