49197a471713a502d2e96844484f9be4a0261603
Jianbin Chen <jason.chen@shopify.com>
2022-03-07 23:06:12 -0800

    Create LeetCode-224-Basic-Calculator.java

---
+++ src/LeetCode-224-Basic-Calculator.java
@@ -0,0 +1,38 @@
+class Solution {
+    /*
+    https://leetcode.com/problems/basic-calculator/discuss/62361/Iterative-Java-solution-with-stack
+    */
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+        int number = 0;
+        int sign = 1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                number = 10 * number + (c - '0');
+            } else if (c == '+') {
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            } else if (c == '-') {
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            } else if (c == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (c == ')') {
+                result += sign * number;
+                number = 0;
+                result *= stack.pop();
+                result += stack.pop();
+            }
+        }
+        
+        if (number != 0) result += sign * number;
+        return result;
+    }
+}
